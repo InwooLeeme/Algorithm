7d97660e3a73a464c9f857f1e61de819436c7a23
ItsmeLee <arsmagica90@gmail.com>
2021-05-01 14:32:39 +0900

    Solve 15552
    Solved 15552 using c.

---
+++ 백준 온라인 저지/15000/15552/15552.c
@@ -0,0 +1,17 @@
+
+// 빠른 A + B
+
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int a = 0, b = 0;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
