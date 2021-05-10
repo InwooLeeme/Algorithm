1d419e51655f37f01f00d5860ae79ce48505e7cf
ItsmeLee <arsmagica90@gmail.com>
2021-05-10 11:44:33 +0900

    Solve 2523
    Solved 2523 using c.

---
+++ 백준 온라인 저지/2000/2523/2523.c
@@ -0,0 +1,25 @@
+// 11024번 : 별 찍기 - 13
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
