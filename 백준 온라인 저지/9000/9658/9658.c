e90a163431e74d4353aea22e62efcc910a9b4214
ItsmeLee <arsmagica90@gmail.com>
2021-05-22 10:49:40 +0900

    Solve 9658

---
+++ 백준 온라인 저지/9000/9658/9658.c
@@ -0,0 +1,18 @@
+// 9658번 : 돌 게임 4
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n % 7 == 1 || n % 7 == 3)
+    {
+        printf("CY\n");
+    }
+    else
+    {
+        printf("SK\n");
+    }
+    return 0;
+}
