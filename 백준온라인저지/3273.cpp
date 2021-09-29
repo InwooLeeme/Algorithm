c99d2e1e1e70a2a88bd5d7d81dafa05c12103f7e
ItsmeLee <arsmagica90@gmail.com>
2021-09-29 20:48:29 +0900

    3273번 : 두 수의 합
    Solve 3273번 using two pointer

---
+++ 백준온라인저지/3273.cpp
@@ -0,0 +1,51 @@
+// sol1) 투 포인터
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include<bits/stdc++.h>
+#define X first
+#define Y second
+#define pb push_back
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define sz(v) (int)(v).size()
+#define all(v) v.begin(), v.end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using dbl = double;
+using ldb = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vs = vector<string>;
+using tii = tuple<int,int,int>;
+template<typename T> using wector = vector<vector<T>>;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 7;
+const ll LNF = 1e18 + 7;
+
+int main() {
+	fastio;
+  int n,x; cin >> n;
+  vi v(n);
+  for(int i = 0; i < n; i++) cin >> v[i];
+  cin >> x;
+  sort(all(v));
+  int cnt = 0;
+  int st = 0,en = n - 1;
+  while(st < en){
+    if(st >= n) break;
+    if(v[st] + v[en] == x) cnt++;
+    if(v[st] + v[en] > x) en--;
+    else st++;
+  }
+  cout << cnt << "\n";
+	return 0;
+}
