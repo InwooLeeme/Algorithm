e2c756a8fa2bfa8127be7bc59fb32c4af7f9e648
ItsmeLee <arsmagica90@gmail.com>
2021-10-12 11:02:29 +0900

    2219번 : 보안 시스템 설치

---
+++ 백준온라인저지/2219.cpp
@@ -0,0 +1,74 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+#define pb push_back
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define sz(v) (int)(v).size()
+#define all(v) v.begin(), v.end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using dbl = double;
+using ldb = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vs = vector<string>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 7;
+const ll LNF = 1e18 + 7;
+
+wector<int> dist(201, vector<int>(201, INF));
+
+int main()
+{
+    fastio;
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        dist[i][i] = 0;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        dist[a][b] = min(dist[a][b], c);
+        dist[b][a] = min(dist[b][a], c);
+    }
+    // floyd
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+            {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+    int mn = INF, ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = 0;
+        for (int j = 1; j <= n; j++)
+        {
+            if (dist[i][j] == INF)
+                break;
+            sum += dist[i][j];
+        }
+        if (mn > sum)
+        {
+            mn = sum;
+            ans = i;
+        }
+    }
+    cout << ans << "\n";
+    return 0;
+}
