814b683c3833c8dfc0505590de5d8aeca8d4dd1b
ItsmeLee <arsmagica90@gmail.com>
2022-02-16 11:21:22 +0900

    24481번 : 알고리즘 수업 - 깊이 우선 탐색 3

---
+++ 백준온라인저지/24481.cpp
@@ -0,0 +1,60 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+
+vector<int> adj[100001];
+vector<int> dist(100001, -1);
+
+void dfs(int cur, int depth)
+{
+    dist[cur] = depth;
+    for (auto &nxt : adj[cur])
+    {
+        if (dist[nxt] != -1)
+            continue;
+        dfs(nxt, depth + 1);
+    }
+}
+
+int32_t main()
+{
+    fastio;
+    int n, m, r;
+    cin >> n >> m >> r;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    for (int i = 1; i <= n; i++)
+        sort(all(adj[i]));
+    dfs(r, 0);
+    for (int i = 1; i <= n; i++)
+        cout << dist[i] << "\n";
+}
