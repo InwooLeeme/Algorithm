4b906a224f30aa51eac154007f68f848e836962f
ItsmeLee <arsmagica90@gmail.com>
2022-02-18 15:58:14 +0900

    Solve A,B

---
+++ AtCoder/Mynavi Programming Contest 2021（AtCoder Beginner Contest 201/B.cpp
@@ -0,0 +1,44 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+
+int32_t main()
+{
+    fastio;
+    int n;
+    cin >> n;
+    vector<pair<int, string>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        int h;
+        cin >> s >> h;
+        v[i] = {h, s};
+    }
+    sort(rall(v));
+    cout << v[1].second << "\n";
+}
