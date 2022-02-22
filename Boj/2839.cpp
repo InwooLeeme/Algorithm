14487cd7ad9a3f004c69cb1c85b3b704b5a0802a
ItsmeLee <arsmagica90@gmail.com>
2022-02-22 16:24:38 +0900

    Solve 2839번 : 설탕배달

---
+++ Boj/2839.cpp
@@ -0,0 +1,53 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+
+int32_t main(){
+	fastio;
+	int user;
+	int count = 0; 
+	scanf("%d", &user);
+	while(user > 0){   
+		if(user % 5 == 0){    
+			user -=5;
+			count++;
+		}
+		else if(user % 3 ==0){  
+			user -=3;
+			count++;
+		}
+		else if(user > 5){  
+			user -=5;
+			count++;
+		}
+		else{
+			count = -1;  
+			break;
+		}
+	}
+	printf("%d", count);
+}
