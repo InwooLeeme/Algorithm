1e8defa1e57655aa663f7d862c69e8295ea15a86
ItsmeLee <arsmagica90@gmail.com>
2022-02-23 13:48:57 +0900

    Solve 4592번 : 중복을 제거하자

---
+++ Boj/4592.cpp
@@ -0,0 +1,39 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+
+int32_t main(){
+	fastio;
+	int n;
+	while(cin >> n && n != 0){
+		vector<int> v(n);
+		for(auto& i : v) cin >> i;
+		v.erase(unique(all(v)),v.end());
+		for(const auto& i : v) cout << i << ' ';
+		cout << "$\n";
+	}
+}
