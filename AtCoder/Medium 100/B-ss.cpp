b080989394e45a70fe2d06f74ded8dbf1e2f2f86
ItsmeLee <arsmagica90@gmail.com>
2022-02-24 11:13:48 +0900

    Solve AtCoder ABC B-ss

---
+++ AtCoder/Medium 100/B-ss.cpp
@@ -0,0 +1,42 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+
+int32_t main(){
+	fastio;
+	string s; cin >> s;
+	for(int i = 0; i < s.size() - 1; i++){
+		auto temp = s.substr(0, s.size() - i - 1);
+		if(temp.size() % 2 == 0){
+			string a = temp.substr(0, temp.size() / 2),b = temp.substr(temp.size() / 2, temp.size());
+			if(a == b){
+				cout << temp.size();
+				break;
+			}
+		}
+	}
+}
