51cf75ec1d81f9b9f436042d4c02d1ee64a3e920
ItsmeLee <arsmagica90@gmail.com>
2022-03-04 12:20:25 +0900

    Solve 18353번 : 병사 배치하기

---
+++ Boj/18353.cpp
@@ -0,0 +1,40 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+
+int32_t main(){
+	fastio;
+	int n; cin >> n;
+	vector<int> LIS,v(n);
+	for(auto& i : v) cin >> i;
+	reverse(all(v));
+	for(int i = 0; i < n; i++){
+		if(LIS.empty() || LIS.back() < v[i]) LIS.push_back(v[i]);
+		else *lower_bound(LIS.begin(), LIS.end(), v[i]) = v[i];
+	}
+	cout << n - LIS.size() << "\n";
+}
