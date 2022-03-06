ffed693a57c55a9ba1ec199285fccef10dc958b8
ItsmeLee <arsmagica90@gmail.com>
2022-03-06 22:40:49 +0900

    Solve 14579번 : 덧셈과 곱셈

---
+++ Boj/14579.cpp
@@ -0,0 +1,48 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+int a,b; 
+const int MOD = 14579;
+int Sol(){
+	int ret = 1;
+	for(int i = a; i <= b; i++){
+		ret *= ((i * (i + 1)) / 2);
+		ret %= MOD;
+	}
+	return ret % MOD;
+}
+
+int32_t main(){
+	fastio;
+	cin >> a >> b;
+	cout << Sol() << "\n";
+}
