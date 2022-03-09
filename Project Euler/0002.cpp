817615e121389d6abc5539a447d6061def4cd9ec
ItsmeLee <arsmagica90@gmail.com>
2022-03-09 22:19:41 +0900

    Solve 0002 : 피보나치 수열에서 4백만 이하이면서 짝수인 항의 합

---
+++ Project Euler/0002.cpp
@@ -0,0 +1,49 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const int sz = 4e6;
+ll d[sz + 1];
+
+int32_t main(){
+	fastio;
+	d[0] = 0;
+	d[1] = 1;
+	d[2] = 2;
+	ll ret = 0;
+	for(int i = 3; i <= sz; i++) d[i] = d[i - 1] + d[i - 2];
+	int i = 0;
+	while(d[i] <= sz){
+		if(d[i] % 2 == 0) ret += d[i];
+		i++;
+	}
+	cout << ret << "\n";
+}
