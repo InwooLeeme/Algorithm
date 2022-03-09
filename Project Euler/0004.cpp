7afeb3b78c98b625abf1dab57db04a624d517932
ItsmeLee <arsmagica90@gmail.com>
2022-03-09 22:38:40 +0900

    Solve 0004번 : 세자리 수를 곱해 만들 수 있는 가장 큰 대칭수

---
+++ Project Euler/0004.cpp
@@ -0,0 +1,51 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+int isPal(int x){
+	string a = to_string(x);
+	string b = a;
+	reverse(all(a));
+	return a == b;
+}
+
+int32_t main(){
+	fastio;
+	int a = 999,mx = 0;
+	for(int i = 100; i <= 999; i++){
+		for(int j = i; j <= 999; j++){
+			if(isPal(i * j)){
+				mx = max(mx, i * j);
+			}
+		}
+	}
+	cout << mx << "\n";
+}
