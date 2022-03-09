7f5be35b363531c3119cec98d060c2270bcd3df2
ItsmeLee <arsmagica90@gmail.com>
2022-03-09 22:48:16 +0900

    Solve 0005번(Brute) : 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수

---
+++ Project Euler/0005.cpp
@@ -0,0 +1,45 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+int32_t main(){
+	fastio;
+	for(int i = 2520; ; i++){
+		int cnt = 0;
+		for(int j = 1; j <= 20; j++){
+			if(i % j == 0) cnt++;
+		}
+		if(cnt == 20){
+			cout << i << "\n";
+			break;
+		}
+	}
+}
