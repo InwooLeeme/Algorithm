3cb9dc6bf9a5f1e0e8243de06862ebe7cbd8e0e4
ItsmeLee <arsmagica90@gmail.com>
2022-03-09 19:43:39 +0900

    Solve 11170번 : 0의 개수

---
+++ Boj/11170.cpp
@@ -0,0 +1,49 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+int Sol(int n,int m){
+	int ret = 0;
+	for(int i = n; i <= m; i++){
+		string S = to_string(i);
+		for(auto& j : S) if(j == '0') ret++;
+	}
+	return ret;
+}
+
+int32_t main(){
+	fastio;
+	int t; cin >> t;
+	while(t--){
+		int n,m; cin >> n >> m;
+		cout << Sol(n,m) << "\n";
+	}
+}
