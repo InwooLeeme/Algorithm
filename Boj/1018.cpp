4ea7b32835de8ea27a06d5744e074a95d7550947
ItsmeLee <arsmagica90@gmail.com>
2022-03-12 13:50:19 +0900

    Solve 1018번 : 체스판 다시 칠하기

---
+++ Boj/1018.cpp
@@ -0,0 +1,82 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+string board[50];
+
+string WB[8] = {
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW"
+};
+string BB[8] = {
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB"
+};
+
+int Check(int x, int y){
+	int a = 0,b = 0;
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			if(board[i + x][j + y] != WB[i][j]){
+				a++;
+			}
+			if(board[i + x][j + y] != BB[i][j]) {
+				b++;
+			}
+		}
+	}
+	return min(a, b);
+}
+
+int32_t main(){
+	fastio;
+	int n,m; cin >> n >> m;
+	for(int i = 0; i < n; i++) cin >> board[i];
+	int mn = 13245;
+	for(int i = 0; i + 8 <= n; i++){
+		for(int j = 0; j + 8 <= m; j++){
+			mn = min(mn, Check(i, j));
+		}
+	}
+	cout << mn << "\n";
+}
