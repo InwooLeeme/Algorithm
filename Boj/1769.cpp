fd29b2d13b05361cd7e0d5356627b03692975501
ItsmeLee <arsmagica90@gmail.com>
2022-03-15 16:19:54 +0900

    Solve 1769번 : 3의 배수

---
+++ Boj/1769.cpp
@@ -0,0 +1,53 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+int ret;
+int n; 
+
+void dfs(int depth, string S){
+	if(S.size() == 1){
+		ret = depth;
+		n = stoi(S);
+		return;
+	}
+	int temp = 0;
+	for(const auto& i : S) temp += (i - '0');
+	dfs(depth + 1, to_string(temp));
+}
+
+int32_t main(){
+	fastio;
+	string k; cin >> k;
+	dfs(0, k);
+	cout << ret << "\n";
+	cout << (n % 3 == 0 ? "YES" : "NO") << "\n";
+}
