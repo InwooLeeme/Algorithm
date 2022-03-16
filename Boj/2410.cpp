a11f0b0acc7599bb4495c5736b46f9662c65209d
ItsmeLee <arsmagica90@gmail.com>
2022-03-16 10:55:19 +0900

    Solve 2410번 : 2의 멱수의 합

---
+++ Boj/2410.cpp
@@ -0,0 +1,44 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const int MOD = 1000000000;
+vector<int> d(1000001, 0);
+
+int32_t main(){
+	fastio;
+	int n; cin >> n;
+	d[1] = 1;
+	for(int i = 2; i <= n; i++){
+		d[i] = (i % 2 ? d[i - 1] : d[i - 1] +d[i / 2]) % MOD;
+	}
+	cout << d[n] << "\n";
+}
