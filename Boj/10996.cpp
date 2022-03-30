90c2a00c882ab83c799cbe1023aff9c6e5519227
ItsmeLee <arsmagica90@gmail.com>
2022-03-30 16:21:28 +0900

    Solve 10996번 : 별 찍기 - 21

---
+++ Boj/10996.cpp
@@ -0,0 +1,55 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+void Sol(int x){
+	for(int i = 1; i <= 2*x; i++){
+		if(i % 2 != 0){
+			for(int j = 1; j <= x; j++){
+				if(j % 2 == 0) cout << ' ';
+				else cout << "*";
+			}
+		}
+		else{
+			for(int j = 1; j <= x; j++){
+				if(j % 2 != 0) cout << ' ';
+				else cout << "*";
+			}
+		}
+		cout << "\n";
+	}
+}
+
+int32_t main(){
+	fastio;
+	int n; cin >> n;
+	Sol(n);
+}
