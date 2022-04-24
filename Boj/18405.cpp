54429f0c36d05d5203154e18dc46894971316932
ItsmeLee <arsmagica90@gmail.com>
2022-04-24 13:34:34 +0900

    Solve 18405번 : 경쟁적 전염

---
+++ Boj/18405.cpp
@@ -0,0 +1,121 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+
+#define X first
+#define Y second
+#define int int64_t
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+#define sf1(a) cin >> a
+#define sf2(a,b) cin >> a >> b
+#define sf3(a,b,c) cin >> a >> b >> c
+#define sf4(a,b,c,d) cin >> a >> b >> c >> d
+#define sf5(a,b,c,d,e) cin >> a >> b >> c >> d >> e
+#define sf6(a,b,c,d,e,f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a,b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << ' '
+#define pf4(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '
+#define pf5(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' '
+#define pf6(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a,b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a,b,c) cout << (a) << ' ' << (b) << ' '<< (c) << '\n'
+#define pf4l(a,b,c,d) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << '\n'
+#define pf5l(a,b,c,d,e) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << '\n'
+#define pf6l(a,b,c,d,e,f) cout << (a) << ' ' << (b) << ' '<< (c) << ' '<< (d) << ' '<< (e) << ' ' << (f) << '\n'
+#define pfvec(V) for(auto const &t : V) pf1(t)
+#define pfvecl(V) for(auto const &t : V) pf1(t); pf0l()
+#define rep(i,a,b) for(int i = a; i < b; i++)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+const int dx[4] = { 1,-1,0,0 };
+const int dy[4] = { 0,0,1,-1 };
+const int ddx[8] = { 0,0,1,1,1,-1,-1,-1 }, ddy[8] = { 1,-1,1,0,-1,1,0,-1 };
+const int MOD = 1e9 + 7;
+
+// Basic Function in Math
+ll POW(ll a, ll b, ll MMM) { ll ret = 1; for (; b; b >>= 1, a = (a*a) % MMM)if (b & 1)ret = (ret*a) % MMM; return ret; }
+ll GCD(ll a, ll b) { return b ? GCD(b, a%b) : a; }
+ll LCM(ll a, ll b) { if (a == 0 || b == 0)return a + b; return a / GCD(a, b) * b; }
+// mod inverse
+int minv(int x,int y){
+	int p = MOD - 2;
+	for(; p; p >>= 1){
+		if(p & 1) x = x * y % MOD;
+		y = y * y % MOD;
+	}
+	return x;
+}
+pll EXGCD(ll a, ll b) {
+	if (b == 0) return { 1,0 };
+	auto t = EXGCD(b, a%b);
+	return { t.second,t.first - t.second*(a / b) };
+}
+
+int n,k,s,enX,enY;
+vector<vector<int>> board(201,vector<int>(201, 0));
+queue<pii> Q[2][1001];
+
+int32_t main(){
+	fastio;
+	sf2(n, k);
+	int time = 0;
+	for(int i = 0; i < n; i++)for(int j = 0; j < n; j++){
+		sf1(board[i][j]);
+		if(board[i][j]) Q[0][board[i][j]].push({i, j});
+	}
+	sf3(s,enX,enY); --enX,--enY;
+	while(s--){
+		for(int i = 1; i <= k; i++){
+			while(!Q[0][i].empty()){
+				auto [curX, curY] = Q[0][i].front(); Q[0][i].pop();
+				for(int j = 0; j < 4; j++){
+					auto nx = curX + dx[j];
+					auto ny = curY + dy[j];
+					if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if(board[nx][ny]) continue;
+					board[nx][ny] = i;
+					Q[1][i].push({nx, ny});
+				}
+			}
+		}	
+		for(int i = 1; i <= k; i++){
+			while(Q[1][i].size()){
+				Q[0][i].push(Q[1][i].front()); Q[1][i].pop();
+			}
+		}	
+	}
+	pf1l(board[enX][enY]);
+}
+
+/*////////////////////////////////////////////////////////////////////
+문제 해법		: 
+결정적 깨달음		: 
+시간복잡도		: 
+오답 원인		: 1. 
+				  2.
+*/////////////////////////////////////////////////////////////////////
