bd885e84722773420b556319788b1b627006587a
ItsmeLee <arsmagica90@gmail.com>
2022-10-07 14:38:48 +0900

    Sol 24768

---
+++ Boj/24768.py
@@ -0,0 +1,12 @@
+while 1:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    if a + b == 13:
+        print("Never speak again.")
+    elif a < b:
+        print("Left beehind.")
+    elif a > b:
+        print("To the convention.")
+    elif a == b:
+        print("Undecided.")
