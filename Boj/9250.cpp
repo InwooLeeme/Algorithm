e2d4cb8b34ad99c167d7b7e7f4339ef8816a42be
ItsmeLee <arsmagica90@gmail.com>
2022-11-08 11:29:37 +0900

    Sol 9250

---
+++ Boj/9250.cpp
@@ -0,0 +1,143 @@
+//////////////////////////////////////////////////////////////////////////////
+/* 
+ _____                            ______                             _        
+/  ___|                           | ___ \                           (_)       
+\ `--.  _ __    __ _   ___   ___  | |_/ /  ___  _ __    __ _  _   _  _  _ __  
+ `--. \| '_ \  / _` | / __| / _ \ |  __/  / _ \| '_ \  / _` || | | || || '_ \ 
+/\__/ /| |_) || (_| || (__ |  __/ | |    |  __/| | | || (_| || |_| || || | | |
+\____/ | .__/  \__,_| \___| \___| \_|     \___||_| |_| \__, | \__,_||_||_| |_|
+       | |                                              __/ |                 
+       |_|                                             |___/     
+//////////////////////////////////////////////////////////////////////////////
+ */
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define X first
+#define Y second
+#define int int64_t
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+#define sf1(a) cin >> a
+#define sf2(a, b) cin >> a >> b
+#define sf3(a, b, c) cin >> a >> b >> c
+#define sf4(a, b, c, d) cin >> a >> b >> c >> d
+#define sf5(a, b, c, d, e) cin >> a >> b >> c >> d >> e
+#define sf6(a, b, c, d, e, f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a, b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << ' '
+#define pf4(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' '
+#define pf5(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' '
+#define pf6(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a, b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << '\n'
+#define pf4l(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << '\n'
+#define pf5l(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << '\n'
+#define pf6l(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << '\n'
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define pfvec(V)           \
+for (auto const &t : V) \
+pf1(t)
+#define pfvecl(V)           \
+for (auto const &t : V) \
+pf1(t);             \
+pf0l()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define EACH(x, a) for (auto& x: a)
+
+using ll  = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+
+using vi = vector<int>;
+using vp = vector<pii>;
+using vvi = vector<vi>;
+using vvp = vector<vp>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T max(vector<T> v) { return *max_element(all(v)); }
+
+template<typename T>
+void read(vector<T>& v){
+EACH(i,v) sf1(i);
+}
+
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+const int ddx[8] = {0, 0, 1, 1, 1, -1, -1, -1}, ddy[8] = {1, -1, 1, 0, -1, 1, 0, -1};
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+const int INF = 1e9 + 7;
+
+struct AhoCorasick {
+    int trie[100'001][26]{}, sz = 0;
+    int fail[100'001]{};
+    int term[100'001]{};
+    void Init(const vector<string>& v) {
+        for (auto& i : v) {
+        int p = 0;
+        for (auto& j : i) {
+            if (!trie[p][j - 'a']) trie[p][j - 'a'] = ++sz;
+            p = trie[p][j - 'a'];
+        }
+        term[p] = 1;
+    }
+        queue<int> Q;
+        for (int i = 0; i < 26; i++) if (trie[0][i]) Q.push(trie[0][i]);
+        while (Q.size()) {
+            int p = Q.front(); Q.pop();
+            for (int i = 0; i < 26; i++) if (trie[p][i]) {
+                int q = fail[p];
+                while (q && !trie[q][i]) q = fail[q];
+                fail[trie[p][i]] = q = trie[q][i];
+                term[trie[p][i]] |= term[q];
+                Q.push(trie[p][i]);
+            }
+        }
+    }
+    bool Query(const string& s) {
+        int p = 0;
+        for (auto& i : s) {
+        while (p && !trie[p][i - 'a']) p = fail[p];
+            p = trie[p][i - 'a'];
+            if (term[p]) return 1;
+        }
+        return 0;
+    }
+} Aho;
+
+int32_t main(){
+    fastio;
+    int n; sf1(n);
+    vector<string> v(n);
+    for(auto& i : v) sf1(i);
+    Aho.Init(v);
+    int q; sf1(q);
+    while(q--){
+        string s; sf1(s);
+        cout << (Aho.Query(s) ? "YES" : "NO") << "\n";
+    }
+}
