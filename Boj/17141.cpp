90e048b01fe1f681bd1499ee0fdac9d16bddda51
ItsmeLee <arsmagica90@gmail.com>
2022-11-29 15:06:49 +0900

    Sol 17141 연구소2

---
+++ Boj/17141.cpp
@@ -0,0 +1,156 @@
+//////////////////////////////////////////////////////////////////////////////
+/* 
+ _____                            ______                             _        
+/  ___|                           | ___ \                           (_)       
+\ `--.  _ __    __ _   ___   ___  | |_/ /  ___  _ __    __ _  _   _  _  _ __  
+ `--. \| '_ \  / _` | / __| / _ \ |  __/  / _ \| '_ \  / _` || | | || || '_ \ 
+/\__/ /| |_) || (_| || (__ |  __/ | |    |  __/| | | || (_| || |_| || || | | |
+\____/ | .__/  \__,_| \___| \___| \_|     \___||_| |_| \__, | \__,_||_||_| |_|
+       | |                                              __/ |                 
+       |_|                                             |___/     
+//////////////////////////////////////////////////////////////////////////////
+ */
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define X first
+#define Y second
+#define int int64_t
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+#define sf1(a) cin >> a
+#define sf2(a, b) cin >> a >> b
+#define sf3(a, b, c) cin >> a >> b >> c
+#define sf4(a, b, c, d) cin >> a >> b >> c >> d
+#define sf5(a, b, c, d, e) cin >> a >> b >> c >> d >> e
+#define sf6(a, b, c, d, e, f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a, b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << ' '
+#define pf4(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' '
+#define pf5(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' '
+#define pf6(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a, b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << '\n'
+#define pf4l(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << '\n'
+#define pf5l(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << '\n'
+#define pf6l(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << '\n'
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define pfvec(V)           \
+for (auto const &t : V) \
+pf1(t)
+#define pfvecl(V)           \
+for (auto const &t : V) \
+pf1(t);             \
+pf0l()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define Init(x, y) memset(x, y, sizeof(x));
+#define EACH(x, a) for (auto& x: a)
+
+using ll  = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+
+using vi = vector<int>;
+using vp = vector<pii>;
+using vvi = vector<vi>;
+using vvp = vector<vp>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T max(vector<T> v) { return *max_element(all(v)); }
+
+template<typename T>
+void read(vector<T>& v){
+EACH(i,v) sf1(i);
+}
+
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+const int ddx[8] = {0, 0, 1, 1, 1, -1, -1, -1}, ddy[8] = {1, -1, 1, 0, -1, 1, 0, -1};
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+const int INF = 1e9 + 7;
+
+int n,m,ret = INF;
+int board[55][55];
+bool chk[27];
+vector<pii> virus;
+vector<pii> v;
+
+bool oob(int x,int y){ return (x < 0 || x >= n || y < 0 || y >= n); }
+
+void F(int depth,int st){
+    if(depth == m){
+        // BFS
+        queue<pii> Q;
+        wector<int> dist(n + 1 ,vector<int>(n + 1, -1));
+        for(auto& [a, b] : v){
+            dist[a][b] = 0;
+            Q.push({a, b});
+        }
+        while(!Q.empty()){
+            auto [curX, curY] = Q.front(); Q.pop();
+            for(int dir = 0; dir < 4; dir++){
+                auto nx = curX + dx[dir],ny = curY + dy[dir];
+                if(oob(nx, ny) || board[nx][ny] == 1 || dist[nx][ny] != -1) continue;
+                dist[nx][ny] = dist[curX][curY] + 1;
+                Q.push({nx, ny});
+            }
+        }
+        // Check
+        int mx = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 0 and dist[i][j] == -1) return;
+                mx = max(mx, dist[i][j]);
+            }
+        }
+        ret = min(ret, mx);
+        return;
+    }
+    for(int i = st; i < virus.size(); i++){
+        if(chk[i]) continue;
+        chk[i] = 1;
+        v.push_back({virus[i].first, virus[i].second});
+        F(depth + 1, i + 1);
+        v.pop_back();
+        chk[i] = 0;
+    }
+}
+
+int32_t main(){
+    fastio;
+    sf2(n, m);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            sf1(board[i][j]);
+            if(board[i][j] == 2){
+                virus.push_back({i, j});
+            }
+        }
+    }
+    F(0, 0);
+    cout << (ret == INF ? -1 : ret) << "\n";
+}
