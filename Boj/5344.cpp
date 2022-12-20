12508afff2811183bc461bc76db3ec12aa681087
ItsmeLee <arsmagica90@gmail.com>
2022-12-20 12:19:23 +0900

    Sol 5344

---
+++ Boj/5344.cpp
@@ -0,0 +1,23 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int64_t
+
+int32_t main(){
+    fastio;
+    int T; cin >> T;
+    while (T--){
+        int a,b; cin >> a >> b;
+        cout << __gcd(a , b) << "\n";
+    }    
+    return 0;
+}
