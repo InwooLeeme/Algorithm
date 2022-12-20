c866cb998b9cfe1bf6a3cb2e57f6cdfe3188c841
ItsmeLee <arsmagica90@gmail.com>
2022-12-20 15:00:09 +0900

    Sol 5340

---
+++ Boj/5340.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+a = input().rstrip()
+b = input().rstrip()
+c = input().rstrip()
+d = input().rstrip()
+e = input().rstrip()
+f = input().rstrip()
+print(f"Latitude {len(a)}:{len(b)}:{len(c)}")
+print(f"Longitude {len(d)}:{len(e)}:{len(f)}")
