afe9d19c506ad7417f06931f41513e41c57cf18f
ItsmeLee <arsmagica90@gmail.com>
2022-12-23 09:59:52 +0900

    Sol 2042

---
+++ Boj/2042.cpp
@@ -0,0 +1,48 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int64_t
+
+template<typename T = int64_t, size_t sz = 20, typename F = plus<T>>
+struct SegTree {
+	vector<T> tree; T t; F f{};
+	SegTree(T t = T()) : tree(1 << sz + 1, t), t(t) {}
+	explicit SegTree(T t, const F& f) : tree(1 << sz + 1, t), t(t), f(f) {}
+
+	void Update(int i, T val) {
+		--i |= 1 << sz; tree[i] = val;
+		while (i >>= 1) tree[i] = f(tree[i << 1], tree[i << 1 | 1]);
+	}
+	T Query(int l, int r) {
+		T L = t, R = t; --l |= 1 << sz, --r |= 1 << sz;
+		while (l <= r) {
+			if (l & 1) L = f(L, tree[l++]);
+			if (~r & 1) R = f(tree[r--], R);
+			l >>= 1, r >>= 1;
+		}
+		return f(L, R);
+	}
+};
+
+
+int32_t main(){
+    fastio;
+    SegTree<int_fast64_t,21> ST; 
+    int n,m,q; cin >> n >> m >> q;
+    for(int i = 1,t; i <= n; i++) cin >> t,ST.Update(i, t);
+    for(int i = 0; i < m + q; i++){
+        int t,j,k; cin >> t >> j >> k;
+        if(t == 1) ST.Update(j, k);
+        else cout << ST.Query(j, k) << "\n";
+    }
+    return 0;
+}
