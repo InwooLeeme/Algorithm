081a5b62925581bc1d8e4bcb8251335ba9c4fece
ItsmeLee <arsmagica90@gmail.com>
2022-12-28 11:33:56 +0900

    Sol 10489

---
+++ Boj/10489.cpp
@@ -0,0 +1,26 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+int32_t main(){
+    fastio;
+    int N; cin >> N;
+    stack<int> S;
+    while(N--){
+        int x; cin >> x;
+        if(!S.empty() and (S.top() + x) % 2 == 0) S.pop();
+        else S.push(x);
+    }
+    cout << S.size() << "\n";
+    return 0;
+}
