362a9134e9543c62fa6dccdf0fa0c5ea5692b71a
ItsmeLee <arsmagica90@gmail.com>
2023-01-01 00:43:42 +0900

    5948

---
+++ Boj/5948.cpp
@@ -0,0 +1,27 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+int32_t main(){
+    fastio;
+    int N,cnt = 0; cin >> N;
+    vector<bool> chk(N, 0);
+    while(!chk[N]){
+        chk[N] = 1;
+        N = ((N % 1000) - (N % 10)) / 10;
+        N = N * N;
+        cnt++;
+    }
+    cout << cnt << "\n";
+    return 0;
+}
