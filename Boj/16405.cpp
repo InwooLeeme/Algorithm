4f9b97a624f3d2ad05c5a38332b35b2ec924a7cc
ItsmeLee <arsmagica90@gmail.com>
2023-01-30 13:28:05 +0900

    16405

---
+++ Boj/16405.cpp
@@ -0,0 +1,140 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define X first
+#define Y second
+#define int int_fast64_t
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+#define sf1(a) cin >> a
+#define sf2(a, b) cin >> a >> b
+#define sf3(a, b, c) cin >> a >> b >> c
+#define sf4(a, b, c, d) cin >> a >> b >> c >> d
+#define sf5(a, b, c, d, e) cin >> a >> b >> c >> d >> e
+#define sf6(a, b, c, d, e, f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a, b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << ' '
+#define pf4(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' '
+#define pf5(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' '
+#define pf6(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a, b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << '\n'
+#define pf4l(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << '\n'
+#define pf5l(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << '\n'
+#define pf6l(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << '\n'
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define pfvec(V)           \
+for (auto const &t : V) \
+pf1(t)
+#define pfvecl(V)           \
+for (auto const &t : V) \
+pf1(t);             \
+pf0l()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define Init(x, y) memset(x, y, sizeof(x));
+#define EACH(x, a) for (auto& x: a)
+
+using ll  = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+
+using vi = vector<int>;
+using vp = vector<pii>;
+using vvi = vector<vi>;
+using vvp = vector<vp>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T max(vector<T> v) { return *max_element(all(v)); }
+
+template<typename T>
+void read(vector<T>& v){
+EACH(i,v) sf1(i);
+}
+
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+const int ddx[8] = {0, 0, 1, 1, 1, -1, -1, -1}, ddy[8] = {1, -1, 1, 0, -1, 1, 0, -1};
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+const int INF = 1e9 + 7;
+
+int n,m,p;
+pair<double, double> v[1111];
+
+struct UnionFind { // union-by-rank ver.
+	vector<int> par, rank, cnt;
+	UnionFind(int n) : par(n), rank(n, 1), cnt(n, 1) {
+		iota(par.begin(), par.end(), 0);
+	}
+	int Find(int x) {
+		return x == par[x] ? x : par[x] = Find(par[x]);
+	}
+	bool Union(int a, int b) {
+		a = Find(a), b = Find(b);
+		if (a == b) return 0;
+		if (rank[a] < rank[b]) swap(a, b);
+		par[b] = a;
+		rank[a] += rank[a] == rank[b];
+		cnt[a] += cnt[b];
+		return 1;
+	}
+};
+
+int32_t main(){
+    fastio;
+    sf3(n, m, p);
+    vector<tuple<double,int,int>> e;
+    for(int i = 1; i <= n; i++) sf2(v[i].first, v[i].second);
+    UnionFind UF(n + 1);
+    int cnt = 0;
+    double cost = 0.0;
+    for(int i = 1; i <= m; i++){
+        if(UF.Union(i, 1)){
+            cnt++;
+        }
+    }
+    while(p--){
+        int a,b; sf2(a, b);
+        if(UF.Union(a, b)){
+            cnt++;
+        }
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = i + 1; j <= n; j++){
+            auto dist = sqrt(pow(v[i].first - v[j].first, 2) + pow(v[i].second - v[j].second, 2));
+            e.push_back({dist, i, j});
+        }
+    }
+    sort(all(e));
+    for(auto& [c, a, b] : e){
+        if(UF.Union(a, b)){
+            cost += c;
+            if(++cnt == n - 1) break;
+        }
+    }
+    cout << fixed << setprecision(3) << cost << "\n";
+    return 0;
+}
