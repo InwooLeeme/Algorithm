d879490028a6c1bb78fb35ead26e3020cfe08ef9
ItsmeLee <arsmagica90@gmail.com>
2023-02-08 10:25:42 +0900

    7120

---
+++ Boj/7120.cpp
@@ -0,0 +1,21 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+int32_t main(){
+    fastio;
+    string s; cin >> s;
+    s.erase(unique(s.begin(), s.end()), s.end());
+    cout << s << '\n';
+    return 0;
+}
