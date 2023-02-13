ecb95daa131eb9278020d3b3cb87770da4d5639d
ItsmeLee <arsmagica90@gmail.com>
2023-02-13 12:47:08 +0900

    14925

---
+++ Boj/14925.cpp
@@ -0,0 +1,29 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+int n,m,d[1111][1111];
+
+int32_t main(){
+    fastio;
+    cin >> n >> m;
+    int mx = 0;
+    for(int i = 1; i <= n; i++) for(int j = 1; j <= m; j++){
+        int t; cin >> t;
+        if(t) continue;
+        d[i][j] = min<int>({d[i - 1][j], d[i][j - 1], d[i - 1][j - 1]}) + 1;
+        mx = max<int>(mx, d[i][j]);
+    }
+    cout << mx << "\n";
+    return 0;
+}
