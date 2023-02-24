39d22bd339684dc393814a008338ba2312226365
InwooLeeme <arsmagica90@gmail.com>
2023-02-24 21:03:36 +0900

    Sol 16986

---
+++ Boj/16986.cpp
@@ -0,0 +1,140 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define X first
+#define Y second
+#define int int_fast64_t
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define SZ(v) (int)(v).size()
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+#define sf1(a) cin >> a
+#define sf2(a, b) cin >> a >> b
+#define sf3(a, b, c) cin >> a >> b >> c
+#define sf4(a, b, c, d) cin >> a >> b >> c >> d
+#define sf5(a, b, c, d, e) cin >> a >> b >> c >> d >> e
+#define sf6(a, b, c, d, e, f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a, b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << ' '
+#define pf4(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' '
+#define pf5(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' '
+#define pf6(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a, b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << '\n'
+#define pf4l(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << '\n'
+#define pf5l(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << '\n'
+#define pf6l(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << '\n'
+#define pfvec(V)            \
+	for (auto const &t : V) \
+	pf1(t)
+#define pfvecl(V)           \
+	for (auto const &t : V) \
+		pf1(t);             \
+	pf0l()
+#define pfvecll(V)           \
+	for (auto const &t : V) \
+		pf1l(t);             \
+	pf0l()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rep1(i,n) for(int i=1;i<=(n);i++)
+#define EACH(x, a) for (auto& x: a)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using tii = tuple<int, int, int>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T max(vector<T> v) { return *max_element(all(v)); }
+template<typename T> T min(vector<T> v) { return *min_element(all(v)); }
+template<typename T> T sz(vector<T> v) { return (int)v.size(); }
+
+template<typename T>
+void read(vector<T>& v){
+	EACH(i,v) sf1(i);
+}
+
+template<typename T>
+void print(T arg) {
+	cout << arg << '\n';
+}
+
+template<typename T, typename... Types>
+void print(T arg, Types... args) {
+	cout << arg << " ";
+	print(args...);
+}
+
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+const int ddx[8] = {0, 0, 1, 1, 1, -1, -1, -1}, ddy[8] = {1, -1, 1, 0, -1, 1, 0, -1};
+const int MOD = 1e9 + 7;
+const long double EPS = 1e-12;
+const int INF = 0x3f3f3f3f;
+const double PI = acos(-1);
+
+int arr[4][21],board[10][10],n,m;
+
+bool Sol(){
+	int win[4] = {};
+	int cnt[4] = {};
+	int p1 = 1,p2 = 2,p3 = 3; // 지우, 경희, 민호
+	while(1){
+		if(cnt[1] > n) return 0;
+		if(win[1] == m) return 1;
+		if(win[2] == m or win[3] == m) return 0;
+		if(p1 > p2) swap(p1, p2);
+		cnt[p1]++;
+		cnt[p2]++;
+		if(board[arr[p1][cnt[p1]]][arr[p2][cnt[p2]]] == 2){
+			win[p1]++;
+			swap(p2, p3);
+		}
+		else{
+			win[p2]++;
+			swap(p1, p3);
+		}
+	}
+}
+
+int32_t main(){
+	fastio;
+	// Input
+	sf2(n, m);
+	for(int i = 1; i <= n; i++) for(int j = 1; j <= n; j++) sf1(board[i][j]);
+	for(int i = 1; i <= 20; i++) arr[1][i] = i;
+	for(int i = 1; i <= 20; i++) sf1(arr[2][i]);
+	for(int i = 1; i <= 20; i++) sf1(arr[3][i]);
+	do{
+		if(Sol()){
+			pf1l(1);
+			return 0;
+		}
+	}while(next_permutation(arr[1] + 1, arr[1] + n + 1));
+	pf1l(0);
+	return 0; 
+}
