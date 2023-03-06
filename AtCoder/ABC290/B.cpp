cbd842f0cae4a55fadaebab1647a388336506d57
ItsmeLee <arsmagica90@gmail.com>
2023-03-06 12:59:01 +0900

    ABC 290

---
+++ AtCoder/ABC290/B.cpp
@@ -0,0 +1,31 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+int32_t main(){
+    fastio;
+    int n,q; cin >> n >> q;
+    string a,b = ""; cin >> a;
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] == 'o' and cnt < q){
+            b += "o";
+            cnt++;
+            continue;
+        }
+        else if(a[i] == 'o' and cnt >= q) b += 'x';
+        else b += 'x';
+    }
+    cout << b;
+    return 0;
+}
