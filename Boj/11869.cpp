0692b0d8423e1e9e66d26d82d4d71557dedbde8a
ItsmeLee <arsmagica90@gmail.com>
2023-05-17 14:41:31 +0900

    11869

---
+++ Boj/11869.cpp
@@ -0,0 +1,24 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int g = 0;
+    int n; cin >> n;
+    while(n--){
+        int t; cin >> t;
+        g ^= t;
+    }
+    cout << (g ? "koosaga" : "cubelover") << "\n";
+    return 0;
+}
