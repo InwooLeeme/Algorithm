0b4c1d86076d81364e4c5ff2ab0c4378be5be63e
ItsmeLee <arsmagica90@gmail.com>
2023-05-31 10:59:17 +0900

    11180

---
+++ Boj/11180.cpp
@@ -0,0 +1,59 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+// 1 - indexed base // 
+struct BM{
+	int n;
+	vector<vector<int>> adj;
+	vector<int> par;
+	vector<bool> match;
+	
+	BM(int n = 0): n(n),adj(n + 1),par(n + 1, -1),match(n + 1, 0){}
+	void AddEdge(int a,int b){ adj[a].push_back(b);}
+	
+	int DFS(int cur){
+		for(auto nxt : adj[cur]){
+			if(match[nxt]) continue;
+			match[nxt] = 1;
+			if(par[nxt] == -1 or DFS(par[nxt])){
+				par[nxt] = cur;
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	int Matching(){
+		int ret = 0;
+		for(int i = 1; i <= n; i++){
+			fill(match.begin(), match.end(), 0);
+			ret += DFS(i);
+		}
+		return ret;
+	}
+};
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n,m; cin >> n >> m;
+    BM G(n);
+    while(m--){
+        int a,b; cin >> a >> b;
+        G.AddEdge(a, b);
+        G.AddEdge(b, a);
+    }
+    int ret = G.Matching();
+    if(ret != n) cout << "Impossible" << '\n';
+    else for(int i = 1; i <= n; i++) cout << G.par[i] << "\n";
+    return 0;
+}
