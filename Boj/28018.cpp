7cd4979822e45764a648e97e0e7103093598697d
ItsmeLee <arsmagica90@gmail.com>
2023-06-07 15:32:34 +0900

    28018

---
+++ Boj/28018.cpp
@@ -0,0 +1,29 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n,m; cin >> n;
+    vector<int> v(1'000'011);
+    for(int i = 0; i < n; i++){
+        int s,e; cin >> s >> e;
+        v[s]++,v[e + 1]--;
+    }
+    for(int i = 1; i < v.size(); i++) v[i] += v[i - 1];
+    cin >> m;
+    while(m--){
+        int t; cin >> t;
+        cout << v[t] << "\n";
+    }
+    return 0;
+}
