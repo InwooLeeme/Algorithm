5d1b2565cccd10914542d9ca1768a829a739343a
InwooLeeme <arsmagica90@gmail.com>
2023-06-23 19:59:21 +0900

    Create 25824.cpp

---
+++ Boj/25824.cpp
@@ -0,0 +1,167 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define X first
+#define Y second
+#define int int_fast64_t
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define Compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+#define IDX(v, x) (lower_bound(all(v), x) - (v).begin())
+#define SZ(x) (int)(x).size()
+#define sf1(a) cin >> a
+#define sf2(a, b) cin >> a >> b
+#define sf3(a, b, c) cin >> a >> b >> c
+#define sf4(a, b, c, d) cin >> a >> b >> c >> d
+#define sf5(a, b, c, d, e) cin >> a >> b >> c >> d >> e
+#define sf6(a, b, c, d, e, f) cin >> a >> b >> c >> d >> e >> f
+#define pf1(a) cout << (a) << ' '
+#define pf2(a, b) cout << (a) << ' ' << (b) << ' '
+#define pf3(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << ' '
+#define pf4(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' '
+#define pf5(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' '
+#define pf6(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << ' '
+#define pf0l() cout << '\n';
+#define pf1l(a) cout << (a) << '\n'
+#define pf2l(a, b) cout << (a) << ' ' << (b) << '\n'
+#define pf3l(a, b, c) cout << (a) << ' ' << (b) << ' ' << (c) << '\n'
+#define pf4l(a, b, c, d) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << '\n'
+#define pf5l(a, b, c, d, e) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << '\n'
+#define pf6l(a, b, c, d, e, f) cout << (a) << ' ' << (b) << ' ' << (c) << ' ' << (d) << ' ' << (e) << ' ' << (f) << '\n'
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define pfvec(V)           \
+for (auto const &t : V) \
+pf1(t)
+#define pfvecl(V)           \
+for (auto const &t : V) \
+pf1(t);             \
+pf0l()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define Init(x, y) memset(x, y, sizeof(x));
+#define EACH(x, a) for (auto& x: a)
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+
+using ll  = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using tii = tuple<int, int, int>;
+
+using vi = vector<int>;
+using vp = vector<pii>;
+using vvi = vector<vi>;
+using vvp = vector<vp>;
+template <typename T>
+using wector = vector<vector<T>>;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T max(vector<T> v) { return *max_element(all(v)); }
+template<typename T> T min(vector<T> v) { return *min_element(all(v)); }
+
+template<typename T>
+void read(vector<T>& v){
+EACH(i,v) sf1(i);
+}
+
+template<typename T>
+void debug(T arg){
+    cout << arg << "\n";
+}
+template<typename T, typename... Types>
+void debug(T arg, Types... args) {
+   cout << arg << "\n";
+   debug(args...);
+}
+
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+const int ddx[8] = {0, 0, 1, 1, 1, -1, -1, -1}, ddy[8] = {1, -1, 1, 0, -1, 1, 0, -1};
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+const int INF = 0x7f7f7f7f;
+
+int32_t main(){
+	fastio;
+	wector<int> v(12, vi(12));
+	for(auto& i : v) read(i);
+	int mn = INF,cur = 0;
+	auto Add = [&](int f, int t) -> int{
+		int ret = 0;
+		ret += v[f][t];
+		return ret;
+	};
+	rep(a, 0, 2){
+		rep(b, 2, 4){
+			rep(c, 4, 6){
+				rep(d, 6, 8){
+					rep(e, 8, 10){
+						rep(f, 10, 12){
+							if(a == 0){
+								cur += Add(a, a + 1);
+								cur += Add(a + 1, b);
+							}
+							if(a == 1){
+								cur += Add(a, a - 1);
+								cur += Add(a - 1, b);
+							}
+							if(b == 2){
+								cur += Add(b, b + 1);
+								cur += Add(b + 1, c);								
+							}
+							if(b == 3){
+								cur += Add(b, b - 1);
+								cur += Add(b - 1, c);
+							}
+							if(c == 4){
+								cur += Add(c, c + 1);
+								cur += Add(c + 1, d);
+							}
+							if(c == 5){
+								cur += Add(c, c - 1);
+								cur += Add(c - 1, d);
+							}
+							if(d == 6){
+								cur += Add(d, d + 1);
+								cur += Add(d + 1, e);
+							}
+							if(d == 7){
+								cur += Add(d, d - 1);
+								cur += Add(d - 1, e);
+							}
+							if(e == 8){
+								cur += Add(e, e + 1);
+								cur += Add(e + 1, f);
+							}
+							if(e == 9){
+								cur += Add(e, e - 1);
+								cur += Add(e - 1, f);
+							}
+							if(f == 10) cur += Add(f, f + 1);						
+							if(f == 11) cur += Add(f, f - 1);
+							mn = min<int>(mn, cur);
+							cur = 0;
+						}
+					}
+				}
+			}
+		}
+	}
+	pf1l(mn);
+	return 0;
+}
