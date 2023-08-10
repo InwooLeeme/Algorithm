b31b707aafec03537cafdfc6828a42c851d6350b
InwooLeeme <arsmagica90@gmail.com>
2023-08-10 16:26:02 +0900

    Create 1506.cpp

---
+++ Boj/1506.cpp
@@ -0,0 +1,75 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+const int PRECISION = 0;
+
+struct SCC {
+	int n, scc_cnt, dfs_cnt;
+	vector<vector<int>> adj;
+	vector<int> scc, dfs_order;
+	stack<int> S;
+
+	SCC(int n) :
+		n(n), scc_cnt(0), dfs_cnt(0),
+		adj(n + 1), scc(n + 1), dfs_order(n + 1) {
+	}
+
+	void AddEdge(int a, int b) { adj[a].push_back(b); }
+
+	int DFS(int cur) {
+		int ret = dfs_order[cur] = ++dfs_cnt; S.push(cur);
+		for (const auto& nxt : adj[cur]) {
+			if (!dfs_order[nxt]) ret = min(ret, DFS(nxt));
+			else if (!scc[nxt]) ret = min(ret, dfs_order[nxt]);
+		}
+		if (ret == dfs_order[cur]) {
+			for (scc_cnt++; S.size();) {
+				int t = S.top(); S.pop();
+				scc[t] = scc_cnt;
+				if (t == cur) break;
+			}
+		}
+		return ret;
+	}
+
+	void GetSCC() {
+		for (int i = 1; i <= n; i++) if (!dfs_order[i]) DFS(i);
+	}
+};
+
+void Main(){
+	int n; cin >> n; 
+	SCC G(n);
+	vector<int> cost(n + 1); for(int i = 1; i <= n; i++) cin >> cost[i];
+	for(int i = 1; i <= n; i++) for(int j = 1; j <= n; j++){
+		char c; cin >> c;
+		if(c == '1') G.AddEdge(i, j);
+	}
+	G.GetSCC(); 
+	vector<vector<int>> g(G.scc_cnt);
+	for (int i = 1; i <= n; i++) g[G.scc[i] - 1].push_back(i);
+	int ret = 0;
+	for(auto& I : g){
+		int mn = int(2e9);
+		for(auto& j : I) mn = min<int>(mn, cost[j]);
+		ret += mn;
+	}
+	cout << ret << "\n";
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
