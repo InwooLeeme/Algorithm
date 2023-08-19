a5ed139b316b00cabfc8b4b9f13ba41230ca78d8
ItsmeLee <arsmagica90@gmail.com>
2023-08-19 22:36:41 +0900

    2533

---
+++ Boj/2533.cpp
@@ -0,0 +1,46 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+const int PRECISION = 0;
+vector<int> G[1'000'010];
+vector<vector<int>> D(1'000'010,vector<int>(2));
+bitset<1'000'010> vist;
+int n;
+
+void Sol(int cur){
+    vist[cur] = 1;
+    for(auto nxt : G[cur]){
+        if(vist[nxt]) continue;
+        Sol(nxt);
+    }
+    D[cur][0] = 0;
+    D[cur][1] = 1;
+    for(auto nxt : G[cur]){
+        D[cur][0] += D[nxt][1];
+        D[cur][1] += min<int>(D[nxt][0], D[nxt][1]);
+    }
+}
+
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    cin >> n;
+    for(int i = 1; i < n; i++){
+        int a,b; cin >> a >> b;
+        G[a].push_back(b); G[b].push_back(a);
+    }
+    Sol(1);
+    cout << min<int>(D[1][1], D[1][0]) << "\n";
+    return 0;
+}
