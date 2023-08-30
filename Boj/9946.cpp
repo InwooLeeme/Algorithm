535f999a4b6e3cf44f01d6e57da2d23f8b3d5b0f
ItsmeLee <arsmagica90@gmail.com>
2023-08-30 10:23:25 +0900

    9946

---
+++ Boj/9946.cpp
@@ -0,0 +1,31 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+const int PRECISION = 0;
+
+void Main(){
+    int t = 1;
+    string a,b; while(cin >> a >> b and a != "END"){
+        sort(a.begin(), a.end()); sort(b.begin(), b.end());
+        cout << "Case " << t << ": ";
+        cout << (a == b ? "same" : "different") << "\n";
+        t++;
+    }    
+}
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
