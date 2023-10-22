d7bd5bb17d1b978145d45310fafd62e0850f5e47
InwooLeeme <arsmagica90@gmail.com>
2023-10-22 20:40:27 +0900

    Create 2580.cpp

---
+++ Boj/2580.cpp
@@ -0,0 +1,349 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int,int>;
+using vp = vector<pii>;
+using vvp = vector<vp>;
+using vs = vector<string>;
+using vvs = vector<vs>;
+using ti3 = tuple<int,int,int>;
+using vti3 = vector<ti3>;
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T, typename V, class cmp = less<T>>
+using ordered_map = tree<T, V, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T>
+using multi_ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template<typename T>
+void printv(vector<T> v) {
+	for (auto e : v) {
+		cout << e << " ";
+	}   cout << "\n";
+}
+ 
+template<typename T>
+void printvv(vector<T> vv) {
+	for (int i=0; i<vv.size(); i++) {
+		cout << i << ": ";
+		for (auto e : vv[i]) {
+			cout << e << " ";
+		}   cout << "\n";
+	}
+}
+
+template<typename T>
+void ri(T &x) {
+	cin >> x;
+}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) {
+	ri(x);
+	ri(args...) ;
+}
+template<typename T>
+void ri(vector<T> &v) {
+	for (auto &x : v) {
+		cin >> x;
+	}
+}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {
+	ri(v);
+	ri(args...);
+}
+
+template<typename T>
+void po(T x) {
+	cout << x << "\n";
+}
+template<typename T, typename... Args>
+void po(T x, Args... args) {
+	cout << x << " ";
+	po(args...) ;
+}
+template<typename T>
+void po(vector<T> &a) {
+	for (int i=0; i<(int)a.size(); i++) {
+		if (i <(int)a.size()-1) {
+			cout << a[i] << " ";
+		}
+		else cout << a[i] << "\n";
+	}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+int cnt_leq_x(vi &a, int x) {
+	return upper_bound(a.begin(), a.end(), x) - a.begin();
+}
+
+int cnt_leq_x(vi &a, int x, int lo, int hi) {
+	return upper_bound(a.begin()+lo, a.begin()+hi, x) - a.begin()+lo;
+}
+
+int cnt_lt_x(vi &a, int x) {
+	return lower_bound(a.begin(), a.end(), x) - a.begin();
+}
+
+int cnt_lt_x(vi &a, int x, int lo, int hi) {
+	return lower_bound(a.begin()+lo, a.begin()+hi, x) - a.begin()+lo;
+}
+
+int cnt_geq_x(vi &a, int x) {
+	return a.end() - lower_bound(a.begin(), a.end(), x);
+}
+
+int cnt_geq_x(vi &a, int x, int lo, int hi) {
+	return a.begin()+hi - lower_bound(a.begin()+lo, a.begin()+hi, x);
+}
+
+int cnt_gt_x(vi &a, int x) {
+	return a.end() - upper_bound(a.begin(), a.end(), x);
+}
+
+int cnt_gt_x(vi &a, int x, int lo, int hi) {
+	return a.begin()+hi - upper_bound(a.begin()+lo, a.begin()+hi, x);
+}
+
+bool mul_overflow(int a, int b) {
+	int c;
+	return __builtin_mul_overflow(a, b, &c);
+}
+
+template<typename T>
+int popcount(T x) {return __builtin_popcount(x);}
+
+template<typename T>
+T sum(vector<T> &a) {
+	T ret = 0;
+	for (auto v : a) ret += v;
+	return ret;
+}
+
+template<typename T>
+T max(vector<T> &a) {
+	return *max_element(a.begin(), a.end());
+}
+
+template<typename T>
+T min(vector<T> &a) {
+	return *min_element(a.begin(), a.end());
+}
+
+int ceil_div(int a, int b) {
+	return (a + b - 1) / b;
+}
+
+int int_pow(int base, int exp) {
+	int res = 1;
+	while (exp) {
+		if (exp & 1) res *= base;
+		exp >>= 1;
+		base *= base;
+	}
+	return res;
+}
+
+int highest_power_of_2(int n) {
+	while((n & (n-1)) != 0){
+		n = n & (n-1);
+	}
+	return n;
+}
+
+int msb_pos(int x) {
+	if (x==0) return -1;
+	int y = __builtin_clzll(x);
+	int ret = 63 - y;
+	return ret;
+}
+
+template<typename T>
+void chmax(T &x, T y) {x = max(x,y);}
+
+template<typename T>
+void chmin(T &x, T y) {x = min(x,y);}
+
+template<typename T>
+void asort(vector<T> &a) {sort(a.begin(), a.end());}
+
+template<typename T>
+void dsort(vector<T> &a) {sort(a.rbegin(), a.rend());}
+
+template<typename T>
+void reverse(vector<T> &a) {reverse(a.begin(), a.end());}
+
+template<typename T>
+set<T> get_set(vector<T> &a) {
+	set<T> ret(a.begin(), a.end());
+	return ret;
+}
+
+template<typename T>
+vector<T> get_unique(vector<T> a) {
+	asort(a);
+	a.erase(unique(a.begin(), a.end()), a.end());
+	return a;
+}
+
+vs Split(string& s){
+	string temp; 
+	vs ret;
+	stringstream in(s);
+	while(in >> temp) ret.push_back(temp);
+	return ret;
+}
+
+int LIS(vi& v){
+	vi LIS;
+	for(auto& i : v){
+		if(LIS.empty() or LIS.back() < i) LIS.push_back(i);
+		else *lower_bound(LIS.begin(), LIS.end(), i) = i;
+	}
+	return LIS.size();
+}
+
+int LDS(vi& v){
+	vi LDS;
+	for(auto& i : v){
+		auto t = -i;
+		if(LDS.empty() or LDS.back() < t) LDS.push_back(t);
+		else *lower_bound(LDS.begin(), LDS.end(), t) = t;
+	}
+	return LDS.size();
+}
+
+int _pow(int x, int n){
+    int ret = 1;
+    while(n){
+        if(n & 1) ret = ret * x;
+        x = x * x;
+        n >>= 1;
+    }
+    return ret;
+}
+
+constexpr int INF = int(2e18);
+
+const int PRECISION = 0;
+
+void finished(){ exit(0); }
+
+int N = 9,cnt;
+int board[9][9];
+bool check[9][9][10];
+pii M[88];
+
+pii Edge(int x, int y){
+	if(0 <= x and x <= 2){
+		if(0 <= y and y <= 2) return {0, 0};
+		else if(3 <= y and y <= 5) return {0, 3};
+		else if(6 <= y and y <= 8) return {0, 6};
+	}
+	else if(3 <= x and x <= 5){
+		if(0 <= y and y <= 2) return {3, 0};
+		else if(3 <= y and y <= 5) return {3, 3};
+		else if(6 <= y and y <= 8) return {3, 6};
+	}
+	else{
+		if(0 <= y and y <= 2) return {6, 0};
+		else if(3 <= y and y <= 5) return {6, 3};
+		else if(6 <= y and y <= 8) return {6, 6};
+	}
+}
+
+bool Check(int x, int y, int cur){
+	// right
+	for(int i = 0; i < N; i++) if(board[x][i] == cur) return 0;
+	// down
+	for(int i = 0; i < N; i++) if(board[i][y] == cur) return 0;
+	// 3x3
+	auto [s, e] = Edge(x, y);
+	for(int i = s; i <= s + 2; i++) for(int j = e; j <= e + 2; j++) if(board[i][j] == cur) return 0;	
+	return 1;
+}
+
+void F(int dep){
+	if(dep == cnt){
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < N; j++){
+				cout << board[i][j] << " ";
+			}
+			cout << "\n";
+		}
+		finished();
+		return;
+	}
+	auto& [x, y] = M[dep];
+	for(int cur = 1; cur <= N; cur++){
+		if(Check(x, y, cur)){
+			board[x][y] = cur;
+			F(dep + 1);
+			board[x][y] = 0;
+		}
+	}
+}
+
+void Main(){
+	for(int i = 0; i < N; i++)for(int j = 0; j < N; j++){
+		ri(board[i][j]);
+		if(!board[i][j]) M[cnt++] = {i, j};
+	}
+	F(0);
+}
+
+int32_t main(){
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	cout.setf(ios::fixed); cout.precision(PRECISION);
+	Main();
+	return 0;
+}
