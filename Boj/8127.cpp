29994f02ddc6c42bf512aca7864ae7a0345504cf
InwooLeeme <66399601+InwooLeeme@users.noreply.github.com>
2024-10-22 21:13:57 +0900

    Create 8127.cpp

---
+++ Boj/8127.cpp
@@ -0,0 +1,238 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define pb push_back
+template<typename T>
+void printv(vector<T> v) {for (auto e : v) { cout << e << " "; }  cout << "\n";} 
+template<typename T>
+void printvv(vector<T> vv) {for (int i=0; i<vv.size(); i++) {cout << i << ": "; for (auto e : vv[i]) {cout << e << " ";}cout << "\n";}}
+template<typename T>
+void ri(T &x) {cin >> x;}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) { ri(x);ri(args...) ;}
+template<typename T>
+void ri(vector<T> &v) {for (auto &x : v) {cin >> x;}}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {ri(v);ri(args...);}
+template<typename T>
+void po(T x) {cout << x << "\n";}
+template<typename T, typename... Args>
+void po(T x, Args... args) {cout << x << " ";po(args...) ;}
+template<typename T>
+void po(vector<T> &a) {
+for (int i=0; i<(int)a.size(); i++) {
+if (i <(int)a.size()-1) {
+cout << a[i] << " ";}
+else cout << a[i] << "\n";
+}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+const int PRECISION = 0;
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, ddy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int nx[8] = {1, 2, 2, 1, -1, -2, -2, -1}, ny[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+constexpr int INF = int(2e18);
+
+struct MCMF {
+    struct Edge {
+        int nxt;
+        int inv; //inverse edge index
+        int res; //residual
+        int cost; //cost
+    };
+
+    vector<vector<Edge>> adj;
+    vector<int> h, inq; //johnson's algorithm, spfa
+    vector<int> dist; //dijkstra
+    vector<int> check, work; //max flow
+    int sz, s, e; //source, sink
+
+    MCMF(int sz) :
+        adj(sz), h(sz), inq(sz),
+        dist(sz), check(sz), work(sz),
+        sz(sz), s(-1), e(-1) {
+    }
+
+    void set_source(int t) { s = t; }
+    void set_sink(int t) { e = t; }
+    void add_edge(int a, int b, int fl, int cost) {
+        Edge forward{ b, (int)adj[b].size(), fl, cost };
+        Edge reverse{ a, (int)adj[a].size(), 0, -cost };
+        adj[a].push_back(forward);
+        adj[b].push_back(reverse);
+    }
+    void init() {
+        fill(h.begin(), h.end(), INF);
+        fill(dist.begin(), dist.end(), INF);
+
+        //johnson's algorithm with spfa
+        queue<int> Q;
+        inq[s] = 1;
+        Q.push(s);
+        while (!Q.empty()) {
+            int cur = Q.front(); Q.pop();
+            inq[cur] = 0;
+            for (auto i : adj[cur]) {
+                if (i.res && h[i.nxt] > h[cur] + i.cost) {
+                    h[i.nxt] = h[cur] + i.cost;
+                    if (!inq[i.nxt]) {
+                        inq[i.nxt] = 1;
+                        Q.push(i.nxt);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < sz; i++) {
+            for (auto& j : adj[i]) {
+                if (j.res) j.cost += h[i] - h[j.nxt];
+            }
+        }
+
+        //get shortest path DAG with dijkstra
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> PQ;
+        dist[s] = 0;
+        PQ.push({ dist[s], s });
+        while (PQ.size()) {
+            auto [cdist, cur] = PQ.top(); PQ.pop();
+            if (dist[cur] ^ cdist) continue;
+            for (auto i : adj[cur]) {
+                if (i.res && dist[i.nxt] > cdist + i.cost) {
+                    dist[i.nxt] = cdist + i.cost;
+                    PQ.push({ dist[i.nxt], i.nxt });
+                }
+            }
+        }
+        for (int i = 0; i < sz; i++) dist[i] += h[e] - h[s];
+    }
+    bool update() { //update shortest path DAG in O(V+E)
+        int mn = INF;
+        for (int i = 0; i < sz; i++) {
+            if (!check[i]) continue;
+            for (const auto& j : adj[i]) {
+                if (j.res && !check[j.nxt]) {
+                    mn = min(mn, dist[i] + j.cost - dist[j.nxt]);
+                }
+            }
+        }
+        if (mn >= INF) return 0;
+        for (int i = 0; i < sz; i++) {
+            if (!check[i]) dist[i] += mn;
+        }
+        return 1;
+    }
+    int dfs(int cur, int fl) {
+        check[cur] = 1;
+        if (cur == e) return fl;
+        for (; work[cur] < adj[cur].size(); work[cur]++) {
+            auto& i = adj[cur][work[cur]];
+            if (!check[i.nxt] && dist[i.nxt] == dist[cur] + i.cost && i.res) {
+                int ret = dfs(i.nxt, min(fl, i.res));
+                if (ret) {
+                    i.res -= ret;
+                    adj[i.nxt][i.inv].res += ret;
+                    return ret;
+                }
+            }
+        }
+        return 0;
+    }
+    pair<int, int> mcmf() { //cost, flow
+        if (s == -1 || e == -1) return { -1, -1 };
+        init();
+        int cost = 0, fl = 0;
+        do {
+            fill(check.begin(), check.end(), 0);
+            fill(work.begin(), work.end(), 0);
+            int cur = 0;
+            while (cur = dfs(s, INF)) {
+                cost += dist[e] * cur;
+                fl += cur;
+                fill(check.begin(), check.end(), 0);
+            }
+        } while (update());
+        return { cost, fl };
+    }
+};
+
+
+
+void Main(){
+	int n; cin >> n;
+	vector<array<int, 4>> v;
+	for(int i = 0; i < n; i++){
+		int m,a,b,c; cin >> m >> a >> b >> c;
+		v.pb({m,a,b,c});
+	}
+	int S = 0, E = (n << 2) + 5;
+	MCMF flow((n << 2) + 10);
+	flow.set_source(S); flow.set_sink(E);
+	auto out = [&](int x) -> int{ return x + n + 1; };
+	for(int i = 1; i <= n; i++) flow.add_edge(S, i, 1, 0);
+	for(int i = 1; i <= n; i++) flow.add_edge(out(i), E, 1, 0);	
+	for(int i = 0; i < n; i++){
+		auto m = v[i][0];
+		auto a = v[i][1];
+		auto b = v[i][2];
+		auto c = v[i][3];
+		for(int j = a; j <= b; j++){
+			auto C = c * abs(m - j);
+			flow.add_edge(i + 1, out(j), 1, C);
+		}
+	}
+	auto ret = flow.mcmf();
+	if(ret.se == n) po(ret.fi);
+	else po("NIE");
+}
+
+int32_t main(){
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	cout.setf(ios::fixed); cout.precision(PRECISION);
+	Main();
+	return 0;
+}
