2a7db666b728cf7fe400a0cd41c2689330878db3
InwooLeeme <66399601+InwooLeeme@users.noreply.github.com>
2024-10-26 23:24:42 +0900

    Create 10256.cpp

---
+++ Boj/10256.cpp
@@ -0,0 +1,181 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define pb push_back
+template<typename T>
+void printv(vector<T> v) {for (auto e : v) { cout << e << " "; }  cout << "\n";} 
+template<typename T>
+void printvv(vector<T> vv) {for (int i=0; i<vv.size(); i++) {cout << i << ": "; for (auto e : vv[i]) {cout << e << " ";}cout << "\n";}}
+template<typename T>
+void ri(T &x) {cin >> x;}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) { ri(x);ri(args...) ;}
+template<typename T>
+void ri(vector<T> &v) {for (auto &x : v) {cin >> x;}}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {ri(v);ri(args...);}
+template<typename T>
+void po(T x) {cout << x << "\n";}
+template<typename T, typename... Args>
+void po(T x, Args... args) {cout << x << " ";po(args...) ;}
+template<typename T>
+void po(vector<T> &a) {
+for (int i=0; i<(int)a.size(); i++) {
+if (i <(int)a.size()-1) {
+cout << a[i] << " ";}
+else cout << a[i] << "\n";
+}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+const int PRECISION = 0;
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, ddy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int nx[8] = {1, 2, 2, 1, -1, -2, -2, -1}, ny[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+struct AhoCorasick {
+	enum {alpha = 26, first = 'A'}; // change this!
+	struct Node {
+		// (nmatches is optional)
+		int back, next[alpha], start = -1, end = -1, nmatches = 0;
+		Node(int v) { memset(next, v, sizeof(next)); }
+	};
+	vector<Node> N;
+	vector<int> backp;
+	void insert(string& s, int j) {
+		assert(!s.empty());
+		int n = 0;
+		for (char c : s) {
+			int& m = N[n].next[Cv(c)];
+			if (m == -1) { n = m = N.size(); N.emplace_back(-1); }
+			else n = m;
+		}
+		if (N[n].end == -1) N[n].start = j;
+		backp.push_back(N[n].end);
+		N[n].end = j;
+		N[n].nmatches++;
+	}
+	AhoCorasick(vector<string>& pat) : N(1, -1) {
+        for(int i = 0; i < pat.size(); i++) insert(pat[i], i);
+		N[0].back = N.size();
+		N.emplace_back(0);
+
+		queue<int> q;
+		for (q.push(0); !q.empty(); q.pop()) {
+			int n = q.front(), prev = N[n].back;
+            for(int i = 0; i < alpha; i++){
+				int &ed = N[n].next[i], y = N[prev].next[i];
+				if (ed == -1) ed = y;
+				else {
+					N[ed].back = y;
+					(N[ed].end == -1 ? N[ed].end : backp[N[ed].start])
+						= N[y].end;
+					N[ed].nmatches += N[y].nmatches;
+					q.push(ed);
+				}
+			}
+		}
+	}
+	vector<int> find(string word) {
+		int n = 0;
+		vector<int> res; // ll count = 0;
+		for (char c : word) {
+			n = N[n].next[Cv(c)];
+			res.push_back(N[n].end);
+			// count += N[n].nmatches;
+		}
+		return res;
+	}
+	vector<vector<int>> findAll(vector<string>& pat, string word) {
+		vector<int> r = find(word);
+		vector<vector<int>> res(word.size());
+        for(int i = 0; i < word.size(); i++){
+			int ind = r[i];
+			while (ind != -1) {
+				res[i - pat[ind].size() + 1].push_back(ind);
+				ind = backp[ind];
+			}
+		}
+		return res;
+	}
+};
+
+vector<string> Split(string s){
+    vector<string> ret;
+    int n = s.size();
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j <= n; j++){
+            reverse(s.begin() + i, s.begin() + j);
+            ret.push_back(s);
+            reverse(s.begin() + i, s.begin() + j);
+        }
+    }
+    return ret;
+}
+
+
+void Main(){
+    int t; ri(t);
+    while(t--){
+        int n,m;  string s,t; ri(n, m, s, t);
+        auto w = Split(t);
+        sort(all(w));
+        w.erase(unique(all(w)), w.end());
+        AhoCorasick Aho(w);
+        int ret = 0;
+        auto R = Aho.findAll(w, s);
+        for(const auto& i : R) ret += (i.empty() ? 0 : 1);
+        po(ret);
+    }
+}   
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
