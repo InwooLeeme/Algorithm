cfeb1e71bbc152f3db17b46b490673e27ea137ae
InwooLeeme <arsmagica90@gmail.com>
2024-12-12 22:50:57 +0900

    Create 3145.cpp

---
+++ Boj/3145.cpp
@@ -0,0 +1,181 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) (int)x.size()
+template<typename T>
+void printv(vector<T> v) {for (auto e : v) { cout << e << " "; }  cout << "\n";} 
+template<typename T>
+void printvv(vector<T> vv) {for (int i=0; i<vv.size(); i++) {cout << i << ": "; for (auto e : vv[i]) {cout << e << " ";}cout << "\n";}}
+template<typename T>
+void ri(T &x) {cin >> x;}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) { ri(x);ri(args...) ;}
+template<typename T>
+void ri(vector<T> &v) {for (auto &x : v) {cin >> x;}}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {ri(v);ri(args...);}
+template<typename T>
+void po(T x) {cout << x << "\n";}
+template<typename T, typename... Args>
+void po(T x, Args... args) {cout << x << " ";po(args...) ;}
+template<typename T>
+void po(vector<T> &a) {
+for (int i=0; i<(int)a.size(); i++) {
+if (i <(int)a.size()-1) {
+cout << a[i] << " ";}
+else cout << a[i] << "\n";
+}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_multimap = tree<T, R, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int PRECISION = 0;
+constexpr int INF = int(2e18);
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, ddy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int nx[8] = {1, 2, 2, 1, -1, -2, -2, -1}, ny[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+
+int n,m;
+string board[55];
+map<int, string> M;
+map<int, pii> I;
+vvi G;
+vi match, l, r;
+
+int DFS(int cur){
+    for(const auto& nxt : G[cur]){
+        if(match[nxt]) continue;
+        match[nxt] = 1;
+        if(l[nxt] == -1 or DFS(l[nxt])){
+            l[nxt] = cur;
+            r[cur] = nxt;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void add_edge(int a, int b){ G[a].pb(b); }
+
+bool Bound(int x, int y){ return x < 0 or x >= n or y < 0 or y >= m; }
+
+void Main(){
+    ri(n, m);
+    for(int i = 0; i < n; i++) ri(board[i]);
+    int cnt1 = 0,cnt2 = 0;
+    vvi v(55, vi(55)),w(55, vi(55));
+    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++){
+        if('A' <= board[i][j] and board[i][j] <= 'Z' and !v[i][j]){
+            cnt1++;
+            v[i][j] = cnt1;
+            string cur = "";
+            while('A' <= board[i][j] and board[i][j] <= 'Z'){
+                cur += board[i][j];
+                v[i][j] = cnt1;
+                j++;
+            }
+            M[cnt1] = cur;
+        }
+    }
+    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++){
+        if(board[i][j] == 'x'){
+            cnt2++;
+            I[cnt2] = {i, j};
+            w[i][j] = cnt2;
+        }
+    }
+    G.resize(cnt2 + 1);
+    match.resize(cnt2 + 1);
+    l.resize(cnt2 + 1);
+    fill(all(l), -1);
+    r.resize(cnt2 + 1);
+    fill(all(r), -1);
+    for(int i = 0; i < n; i++) for(int j = 0; j < m; j++){
+        if(board[i][j] == 'x'){
+            for(int d = 0; d < 8; d++){
+                auto nx = i + ddx[d], ny = j + ddy[d];
+                if(Bound(nx, ny)) continue;
+                if(!v[nx][ny]) continue;
+                add_edge(w[i][j], v[nx][ny]);
+            }
+        }
+    }
+    for(int i = 0; i < SZ(G); i++){
+        sort(all(G[i]));
+        G[i].erase(unique(all(G[i])), G[i].end());
+    }
+    int ret = 0;
+    for(int i = 1; i <= cnt2; i++){
+        fill(all(match), 0);
+        ret += DFS(i);
+    }
+    for(int i = 1; i <= cnt2; i++) po(I[i].fi + 1, I[i].se + 1, M[r[i]]);    
+}
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
