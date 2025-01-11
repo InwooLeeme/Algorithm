e06a4f1ce5485e7cd5cd445289d778fe473a621b
InwooLeeme <arsmagica90@gmail.com>
2025-01-12 01:02:54 +0900

    Create 25827.cpp

---
+++ Boj/25827.cpp
@@ -0,0 +1,153 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) (int)x.size()
+template<typename T>
+void printv(vector<T> v) {for (auto e : v) { cout << e << " "; }  cout << "\n";} 
+template<typename T>
+void printvv(vector<T> vv) {for (int i=0; i<vv.size(); i++) {cout << i << ": "; for (auto e : vv[i]) {cout << e << " ";}cout << "\n";}}
+template<typename T>
+void ri(T &x) {cin >> x;}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) { ri(x);ri(args...) ;}
+template<typename T>
+void ri(vector<T> &v) {for (auto &x : v) {cin >> x;}}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {ri(v);ri(args...);}
+template<typename T>
+void po(T x) {cout << x << "\n";}
+template<typename T, typename... Args>
+void po(T x, Args... args) {cout << x << " ";po(args...) ;}
+template<typename T>
+void po(vector<T> &a) {
+for (int i=0; i<(int)a.size(); i++) {
+if (i <(int)a.size()-1) {
+cout << a[i] << " ";}
+else cout << a[i] << "\n";
+}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_multimap = tree<T, R, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int PRECISION = 0;
+constexpr int INF = int(2e18);
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, ddy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int nx[8] = {1, 2, 2, 1, -1, -2, -2, -1}, ny[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using p = pair<int, int>;
+using ti3 = tuple<int, int, int>;
+
+template<size_t sz>
+struct SegTree {
+	vector<int> tree, lazy;
+	SegTree() : tree(sz << 1), lazy(sz << 1) {}
+	void Push(int node, int L, int R) {
+		if (node < sz) for (const int nxt : { node << 1, node << 1 | 1}) lazy[nxt] += lazy[node];
+		tree[node] += lazy[node] * (R - L + 1), lazy[node] = 0;
+	}
+	void Update(int l, int r, int val, int node = 1, int L = 1, int R = sz) {
+		Push(node, L, R);
+		if (r < L || R < l) return;
+		if (l <= L && R <= r) { lazy[node] += val, Push(node, L, R); return; }
+		int mid = L + R >> 1;
+		Update(l, r, val, node << 1, L, mid);
+		Update(l, r, val, node << 1 | 1, mid + 1, R);
+		tree[node] = tree[node << 1] + tree[node << 1 | 1];
+	}
+	int Query(int l, int r, int node = 1, int L = 1, int R = sz) {
+		Push(node, L, R);
+		if (r < L || R < l) return 0;
+		if (l <= L && R <= r) return tree[node];
+		int mid = L + R >> 1;
+		return Query(l, r, node << 1, L, mid) + Query(l, r, node << 1 | 1, mid + 1, R);
+	}
+};
+
+int N; 
+
+int Split(string s){
+    vector<string> ret;
+    for(auto& i : s) if(i == ':') i = ' ';
+    stringstream in(s);
+    string temp;
+    while(in >> temp) ret.pb(temp);
+    return stoi(ret[0])*3600 + stoi(ret[1]) * 60 + stoi(ret[2]);
+}
+
+void Main(){
+    ri(N);
+    SegTree<1 << 17> ST;
+    for(int i = 0; i < N; i++){
+        int t; string a,b; ri(t, a, b);
+        int S = Split(a), E = Split(b);
+        if(t == 1) ST.Update(S + 1, E, 1);
+        else po(ST.Query(S + 1, E));
+    }
+}   
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
