8aaa18709671f59e35bb7825e35f69eeb6751aef
InwooLeeme <arsmagica90@gmail.com>
2025-01-13 20:14:11 +0900

    Create 16707.cpp

---
+++ Boj/16707.cpp
@@ -0,0 +1,184 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) (int)x.size()
+template<typename T>
+void printv(vector<T> v) {for (auto e : v) { cout << e << " "; }  cout << "\n";} 
+template<typename T>
+void printvv(vector<T> vv) {for (int i=0; i<vv.size(); i++) {cout << i << ": "; for (auto e : vv[i]) {cout << e << " ";}cout << "\n";}}
+template<typename T>
+void ri(T &x) {cin >> x;}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) { ri(x);ri(args...) ;}
+template<typename T>
+void ri(vector<T> &v) {for (auto &x : v) {cin >> x;}}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {ri(v);ri(args...);}
+template<typename T>
+void po(T x) {cout << x << "\n";}
+template<typename T, typename... Args>
+void po(T x, Args... args) {cout << x << " ";po(args...) ;}
+template<typename T>
+void po(vector<T> &a) {
+for (int i=0; i<(int)a.size(); i++) {
+if (i <(int)a.size()-1) {
+cout << a[i] << " ";}
+else cout << a[i] << "\n";
+}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_multimap = tree<T, R, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int PRECISION = 0;
+constexpr int INF = int(2e18);
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, ddy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int nx[8] = {1, 2, 2, 1, -1, -2, -2, -1}, ny[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using p = pair<int, int>;
+using ti3 = tuple<int, int, int>;
+
+// 유량을 k 만큼 흘리는 경우: run(src, snk, k)
+// 유량을 최대한 많이 흘리는 경우: run(src, snk)
+template<typename flow_t=int, typename cost_t=long long, flow_t MAX_U=(1<<30), cost_t MAX_C=(1LL<<60)>
+struct MinCostFlow{ // 0-based
+    struct edge_t{ int v, r; flow_t c; cost_t d; };
+    int n;
+    vector<vector<edge_t>> g;
+    vector<int> prv, idx, chk;
+    vector<cost_t> dst;
+    MinCostFlow(int n) : n(n) { clear(); }
+    void clear(){
+        g = vector<vector<edge_t>>(n);
+        prv = idx = chk = vector<int>(n);
+        dst = vector<cost_t>(n);
+    }
+    void add_edge(int s, int e, flow_t c, cost_t d){
+        g[s].push_back({e, (int)g[e].size(), c, d});
+        g[e].push_back({s, (int)g[s].size()-1, 0, -d});
+    }
+    bool find_path(int s, int t){
+        fill(chk.begin(), chk.end(), 0);
+        fill(dst.begin(), dst.end(), MAX_C);
+        queue<int> que; que.push(s); dst[s] = 0; chk[s] = 1;
+        while(!que.empty()){
+            int v = que.front(); que.pop(); chk[v] = 0;
+            for(int i=0; i<g[v].size(); i++){
+                const auto &e = g[v][i];
+                if(e.c > 0 && dst[e.v] > dst[v] + e.d){
+                    dst[e.v] = dst[v] + e.d; prv[e.v] = v; idx[e.v] = i;
+                    if(!chk[e.v]) que.push(e.v), chk[e.v] = 1;
+                }
+            }
+        }
+        return dst[t] < MAX_C;
+    }
+    pair<flow_t, cost_t> augment(int s, int t, flow_t k=-1){
+        if(!find_path(s, t)) return {0, 0};
+        flow_t fl = MAX_U;
+        for(int i=t; i!=s; i=prv[i]) fl = min(fl, g[prv[i]][idx[i]].c);
+        if(k != -1) fl = min(fl, k);
+        for(int i=t; i!=s; i=prv[i]){
+            g[prv[i]][idx[i]].c -= fl;
+            g[i][g[prv[i]][idx[i]].r].c += fl;
+        }
+        return {fl, fl * dst[t]};
+    }
+    pair<flow_t, cost_t> run(int s, int t, flow_t k=-1){
+        flow_t flow = 0; cost_t cost = 0;
+        while(true){
+            auto [fl,cst] = augment(s, t, k);
+            if(fl == 0) break;
+            flow += fl; cost += cst;
+            if(k != -1) k -= fl;
+        }
+        return {flow, cost};
+    }
+};
+
+int N, M;
+int in(int x){ return x; }
+int out(int x){ return x + N;}
+
+void Main(){
+	ri(N, M);
+	MinCostFlow<int, int> flow((N << 1) + 2);
+	int S = 0, E = (N << 1) + 1;
+	for(int i = 1; i <= N; i++) flow.add_edge(in(i), out(i), INF, 0);
+	for(int i = 0; i < M; i++){
+		int a,b,c; ri(a, b, c);
+		flow.add_edge(out(a), in(b), 1, c);
+		flow.add_edge(out(b), in(a), 1, c);
+	}
+	flow.add_edge(S, in(2), 2, 0);
+	flow.add_edge(out(1), E, 1, 0);
+	flow.add_edge(out(N), E, 1, 0);
+	auto [F, C] = flow.run(S, E);
+	po(C);
+}   
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
