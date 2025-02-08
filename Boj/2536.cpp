bb755180860562df23c4eaf08475fc5dab81b0b6
InwooLeeme <ars_magica@naver.com>
2025-02-08 22:36:12 +0900

    Create 2536.cpp

---
+++ Boj/2536.cpp
@@ -0,0 +1,215 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) (int)x.size()
+template<typename T>
+void printv(vector<T> v) {for (auto e : v) { cout << e << " "; }  cout << "\n";} 
+template<typename T>
+void printvv(vector<T> vv) {for (int i=0; i<vv.size(); i++) {cout << i << ": "; for (auto e : vv[i]) {cout << e << " ";}cout << "\n";}}
+template<typename T>
+void ri(T &x) {cin >> x;}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) { ri(x);ri(args...) ;}
+template<typename T>
+void ri(vector<T> &v) {for (auto &x : v) {cin >> x;}}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {ri(v);ri(args...);}
+template<typename T>
+void po(T x) {cout << x << "\n";}
+template<typename T, typename... Args>
+void po(T x, Args... args) {cout << x << " ";po(args...) ;}
+template<typename T>
+void po(vector<T> &a) {
+for (int i=0; i<(int)a.size(); i++) {
+if (i <(int)a.size()-1) {
+cout << a[i] << " ";}
+else cout << a[i] << "\n";
+}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_multimap = tree<T, R, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int PRECISION = 0;
+constexpr int INF = int(2e18);
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, ddy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int nx[8] = {1, 2, 2, 1, -1, -2, -2, -1}, ny[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using p = pair<int, int>;
+using ti3 = tuple<int, int, int>;
+
+struct point {
+	int x, y;
+	/* constructors */
+	point() : x(0), y(0) {}
+	point(int x, int y) : x(x), y(y) {}
+	/* methods */
+	friend int dist(point a, point b) { // (Euclidean distance)^2, use ll if x, y > 1e4
+		return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+	}
+	friend int ccw(point a, point b, point c) {
+		int t = (b.x - a.x) * (c.y - b.y) - (c.x - b.x) * (b.y - a.y);
+		return t ? t > 0 ? 1 : -1 : 0;
+	}
+	friend int is_cross(point a, point b, point c, point d) { // cross : -1(X), intersect : 1(X or L)
+		int ab = ccw(a, b, c) * ccw(a, b, d);
+		int cd = ccw(c, d, a) * ccw(c, d, b);
+		if (ab < 0 && cd < 0) return -1;
+		if (ab == 0 && cd == 0) {
+			if (a > b) swap(a, b);
+			if (c > d) swap(c, d);
+			if (!ccw(a, b, c) and !ccw(a, b, d) and !ccw(c, d, a) and !ccw(c, d, b) and c < b and a < d) return 2; // intersect INF
+			return c <= b && a <= d;
+		}
+		return ab <= 0 && cd <= 0;
+	}
+	friend point findInterCod(point a, point b, point c, point d){
+		double x = (a.x*b.y - a.y*b.x)*(c.x - d.x) - (a.x - b.x) * (c.x*d.y - c.y*d.x);
+		double y = (a.x*b.y - a.y*b.x)*(c.y - d.y) - (a.y - b.y) * (c.x*d.y - c.y*d.x);
+		double div = (a.x - b.x)*(c.y - d.y) - (a.y - b.y) * (c.x - d.x);
+		if(div == 0.0){
+			if(b == c and a <= c) return b;
+			else if(a == d and a >= c) return a;
+		}
+		return point(x/div, y/div);
+	}
+	/* operators */
+	bool operator == (const point& i) const { return x == i.x && y == i.y; }
+	bool operator <  (const point& i) const { return x < i.x || x == i.x && y < i.y; }
+	bool operator >  (const point& i) const { return i < *this; }
+	bool operator != (const point& i) const { return !(*this == i); }
+	bool operator <= (const point& i) const { return *this < i || *this == i; }
+	bool operator >= (const point& i) const { return *this > i || *this == i; }
+	friend int operator * (point a, point b) { return a.x * b.y - a.y * b.x; } // cross product
+	friend point operator + (point a, point b) { return { a.x + b.x, a.y + b.y }; }
+	friend point operator - (point a, point b) { return { a.x - b.x, a.y - b.y }; }
+	friend istream& operator >> (istream& in, point& i) { in >> i.x >> i.y; return in; }
+};
+
+struct line {
+	point a, b;
+	/* constructors */
+	line() {}
+	line(point a, point b) : a(a), b(b) { if (a > b) swap(a, b); }
+	/* methods */
+	double length() { return sqrt(dist(a, b)); }
+	bool is_on_line(point c) { return !ccw(a, b, c) && min(a, b) <= c && c <= max(a, b); }
+	friend int is_cross(line a, line b) { return is_cross(a.a, a.b, b.a, b.b); }
+	// point to line distance
+	friend double dist(line a, point b) {
+		auto CCW = [&](point& a, point& b, point& c) -> int {
+		    return (b.x - a.x) * (c.y - b.y) - (c.x - b.x) * (b.y - a.y);
+		};
+		return CCW(a.a, a.b, b) / a.length();
+	}
+	/* operators */
+	friend istream& operator >> (istream& in, line& i) { in >> i.a >> i.b; return in; }
+};
+
+int N, M, K;
+
+
+void Main(){
+    ri(N, M, K);
+    vector<line> v(K);
+    for(int i = 0; i < K; i++){
+    	int b; point x,y; ri(b, x, y);
+    	v[i] = {x, y};
+	}
+    point S, E; ri(S, E);
+    min_pq<p> PQ;
+    vi D(K + 1, INF);
+    for(int i = 0; i < K; i++){
+    	auto cur = v[i];
+    	if(cur.is_on_line(S)){
+    		PQ.push({D[i] = 0, i});
+		}
+	}
+	while(SZ(PQ)){
+		auto [cdist, cur] = PQ.top(); PQ.pop();
+		if(D[cur] < cdist) continue;
+		for(int j = 0; j < K; j++){
+			auto nxt = v[j];
+			if(is_cross(v[cur], nxt)){
+				if(D[j] > cdist + 1){
+					D[j] = D[cur] + 1;
+					PQ.push({D[j], j});
+				}
+			}
+		}
+	}
+	int ret = INF;
+	for(int i = 0; i < K; i++){
+		if(D[i] == INF) continue;
+		if(v[i].is_on_line(E)){
+			ret = min<int>(ret, D[i]);
+		}
+	}
+	po(ret + 1);
+}
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
