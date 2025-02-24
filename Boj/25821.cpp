e2150aeae87ed944611725d1b2220590b35dd149
InwooLeeme <ars_magica@naver.com>
2025-02-24 23:11:40 +0900

    Create 25821.cpp

---
+++ Boj/25821.cpp
@@ -0,0 +1,197 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) (int)x.size()
+template<typename T>
+void printv(vector<T> v) {for (auto e : v) { cout << e << " "; }  cout << "\n";} 
+template<typename T>
+void printvv(vector<T> vv) {for (int i=0; i<vv.size(); i++) {cout << i << ": "; for (auto e : vv[i]) {cout << e << " ";}cout << "\n";}}
+template<typename T>
+void ri(T &x) {cin >> x;}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) { ri(x);ri(args...) ;}
+template<typename T>
+void ri(vector<T> &v) {for (auto &x : v) {cin >> x;}}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {ri(v);ri(args...);}
+template<typename T>
+void po(T x) {cout << x << "\n";}
+template<typename T, typename... Args>
+void po(T x, Args... args) {cout << x << " ";po(args...) ;}
+template<typename T>
+void po(vector<T> &a) {
+for (int i=0; i<(int)a.size(); i++) {
+if (i <(int)a.size()-1) {
+cout << a[i] << " ";}
+else cout << a[i] << "\n";
+}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_multimap = tree<T, R, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int PRECISION = 0;
+constexpr int INF = int(2e18);
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, ddy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int nx[8] = {1, 2, 2, 1, -1, -2, -2, -1}, ny[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using p = pair<int, int>;
+using ti3 = tuple<int, int, int>;
+
+using ll = long long;
+using ull = unsigned long long;
+
+struct Random {
+	mt19937 rd;
+	Random() : rd((unsigned)chrono::steady_clock::now().time_since_epoch().count()) {}
+	Random(int seed) : rd(seed) {}
+	template<typename T = int>
+	T GetInt(T l = 0, T r = 32767) {
+		return uniform_int_distribution<T>(l, r)(rd);
+	}
+	double GetDouble(double l = 0, double r = 1) {
+		return uniform_real_distribution<double>(l, r)(rd);
+	}
+} Rand;
+
+struct MillerRabin {
+	ll Mul(ll x, ll y, ll MOD) {
+		ll ret = x * y - MOD * ull(1.L / MOD * x * y);
+		return ret + MOD * (ret < 0) - MOD * (ret >= (ll)MOD);
+	}
+	ll _pow(ll x, ll n, ll MOD) {
+		ll ret = 1; x %= MOD;
+		for (; n; n >>= 1) {
+			if (n & 1) ret = Mul(ret, x, MOD);
+			x = Mul(x, x, MOD);
+		}
+		return ret;
+	}
+	bool Check(ll x, ll p) {
+		if (x % p == 0) return 0;
+		for (ll d = x - 1; ; d >>= 1) {
+			ll t = _pow(p, d, x);
+			if (d & 1) return t != 1 && t != x - 1;
+			if (t == x - 1) return 0;
+		}
+	}
+	bool IsPrime(ll x) {
+		if (x == 2 || x == 3 || x == 5 || x == 7) return 1;
+		if (x % 2 == 0 || x % 3 == 0 || x % 5 == 0 || x % 7 == 0) return 0;
+		if (x < 121) return x > 1;
+		if (x < 1ULL << 32) for (auto& i : { 2, 7, 61 }) {
+			if (x == i) return 1;
+			if (x > i && Check(x, i)) return 0;
+		}
+		else for (auto& i : { 2, 325, 9375, 28178, 450775, 9780504, 1795265022 }) {
+			if (x == i) return 1;
+			if (x > i && Check(x, i)) return 0;
+		}
+		return 1;
+	}
+};
+
+struct PollardRho : public MillerRabin {
+	void Rec(ll n, vector<ll>& v) {
+		if (n == 1) return;
+		if (~n & 1) { v.push_back(2); Rec(n >> 1, v); return; }
+		if (IsPrime(n)) { v.push_back(n); return; }
+		ll a, b, c, g = n;
+		auto f = [&](ll x) { return (c + Mul(x, x, n)) % n; };
+		do {
+			if (g == n) {
+				a = b = Rand.GetInt<ll>(0, n - 3) + 2;
+				c = Rand.GetInt<ll>(0, 19) + 1;
+			}
+			a = f(a); b = f(f(b)); g = __gcd(abs(a - b), n);
+		} while (g == 1);
+		Rec(g, v); Rec(n / g, v);
+	}
+	vector<ll> Factorize(ll n) {
+		vector<ll> ret; Rec(n, ret);
+		sort(ret.begin(), ret.end());
+		return ret;
+	}
+} P;
+
+void Main(){
+	int L, R; ri(L, R);
+	int ret = 0;
+	auto Check = [&](int x) -> bool{
+		return L <= x and x <= R and P.IsPrime(x);
+	};
+	for(int i = 1; i <= int(1e6); i++){
+		string a = to_string(i),b = a;
+		reverse(all(b));
+		int cur = stoll(a + b);
+		int nxt = stoll(a.substr(0, SZ(a) - 1) + b);
+		ret += Check(cur); ret += Check(nxt);
+	}
+	po(ret);
+}
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
