2ef8c031177c533026a336acc209b2cea5f5281a
InwooLeeme <ars_magica@naver.com>
2025-03-27 00:00:49 +0900

    Create 12880.cpp

---
+++ Boj/12880.cpp
@@ -0,0 +1,194 @@
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
+#define int int_fast64_t
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) (int)x.size()
+template<typename T>
+void printv(vector<T> v) {for (auto e : v) { cout << e << " "; }  cout << "\n";} 
+template<typename T>
+void printvv(vector<T> vv) {for (int i=0; i<vv.size(); i++) {cout << i << ": "; for (auto e : vv[i]) {cout << e << " ";}cout << "\n";}}
+template<typename T>
+void ri(T &x) {cin >> x;}
+template<typename T, typename... Args>
+void ri(T &x, Args&... args) { ri(x);ri(args...) ;}
+template<typename T>
+void ri(vector<T> &v) {for (auto &x : v) {cin >> x;}}
+template<typename T, typename... Args>
+void ri(vector<T> &v, Args&... args) {ri(v);ri(args...);}
+template<typename T>
+void po(T x) {cout << x << "\n";}
+template<typename T, typename... Args>
+void po(T x, Args... args) {cout << x << " ";po(args...) ;}
+template<typename T>
+void po(vector<T> &a) {
+for (int i=0; i<(int)a.size(); i++) {
+if (i <(int)a.size()-1) {
+cout << a[i] << " ";}
+else cout << a[i] << "\n";
+}
+}
+
+void __print(int x) {cerr << x;}
+void __print(signed x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T1, typename T2, typename T3>
+void __print(const tuple<T1, T2, T3> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << '}';}
+template<typename T1, typename T2, typename T3, typename T4>
+void __print(const tuple<T1, T2, T3, T4> &x) {cerr << '{'; __print(get<0>(x)); cerr << ','; __print(get<1>(x)); cerr << ','; __print(get<2>(x)); cerr << ','; __print(get<3>(x)); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+template<typename T1, typename T2>
+void __print(map<T1,T2> &mp) {for (auto [k,v] : mp) {cerr << '{'; __print(k); cerr << ':'; __print(v); cerr << '}';}}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+template<typename T>
+using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_pq = priority_queue<T>;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R>
+using ordered_multimap = tree<T, R, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int PRECISION = 0;
+constexpr int INF = int(2e18);
+const int dx[4] = {0, 1, 0, -1}, dy[4] = {-1, 0, 1, 0};
+const int ddx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, ddy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int nx[8] = {1, 2, 2, 1, -1, -2, -2, -1}, ny[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using ti3 = tuple<int, int, int>;
+using vp = vector<pii>;
+
+struct SCC {
+	int n, scc_cnt, dfs_cnt;
+	vector<vector<int>> adj;
+	vector<int> scc, dfs_order;
+	stack<int> S;
+
+	SCC(int n = 0) :
+		n(n), dfs_order(n + 1),
+		adj(n + 1), scc(n + 1),
+		scc_cnt(0), dfs_cnt(0) {}
+
+	void AddEdge(int a, int b) { adj[a].push_back(b); }
+
+	int DFS(int cur) {
+		int ret = dfs_order[cur] = ++dfs_cnt; S.push(cur);
+		for (const auto& nxt : adj[cur]) {
+			if (!dfs_order[nxt]) ret = min(ret, DFS(nxt));
+			else if (!scc[nxt]) ret = min(ret, dfs_order[nxt]);
+		}
+		if (ret == dfs_order[cur]) {
+			for (scc_cnt++; S.size();) {
+				int t = S.top(); S.pop();
+				scc[t] = scc_cnt;
+				if (t == cur) break;
+			}
+		}
+		return ret;
+	}
+
+	void GetSCC() {
+		for (int i = 1; i <= n; i++)
+			if (!dfs_order[i]) DFS(i);
+	}
+
+	vector<int> GetIndegree(){
+		vector<int> ret(scc_cnt + 1);
+		for(int i = 1; i <= n; i++){
+			for(auto& nxt : adj[i]){
+				if(scc[i] == scc[nxt]) continue;
+				ret[scc[nxt]]++;
+			}
+		}
+		return ret;
+	}
+
+	vector<vector<int>> GetDAG(){
+		vector<vector<int>> G(scc_cnt + 1);
+		for(int i = 1; i <= n; i++){
+			for(auto& nxt : adj[i]){
+				if(scc[i] == scc[nxt]) continue;
+				G[scc[i]].push_back(scc[nxt]);
+			}
+		}
+		for(auto& i : G){
+			sort(i.begin(), i.end());
+			i.erase(unique(i.begin(), i.end()), i.end());
+		}
+		return G;
+	}
+};
+
+int n, v[55][55];
+
+void Main(){
+    ri(n);
+    for(int i = 1; i <= n; i++) for(int j = 1; j <= n; j++) ri(v[i][j]);
+    auto Check = [&](const int L, const int R) -> bool{
+        SCC scc(n);
+        for(int i = 1; i <= n; i++) for(int j = 1; j <= n; j++){
+            if(L <= v[i][j] and v[i][j] <= R) scc.AddEdge(i, j);
+        }
+        scc.GetSCC();
+        for(int i = 1; i <= n; i++) if(scc.scc[1] != scc.scc[i]) return 0;
+        return 1;
+    };
+    int mn = INF;
+    for(int i = 1; i <= n; i++) for(int j = 1; j <= n; j++){
+        int ok = v[i][j] - 1, not_ok = 150'002;
+        while(ok + 1 < not_ok){
+            auto mid = ok + not_ok >> 1;
+            if(!Check(v[i][j], mid)) ok = mid;
+            else not_ok = mid;
+        }
+        if(not_ok != 150'002) mn = min<int>(mn, not_ok - v[i][j]);
+    }
+    po(mn);
+}
+
+int32_t main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout.setf(ios::fixed); cout.precision(PRECISION);
+    Main();
+    return 0;
+}
